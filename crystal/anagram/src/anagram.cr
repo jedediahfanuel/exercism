e1476f94a4fcd6ad021445fcad85aad58c624b0a
Jeddi212 <jedediahf51@gmail.com>
2022-06-26 17:53:32 +0700

    rfc(crystal): anagram w/ spec

--- crystal/anagram/src/anagram.cr
+++ crystal/anagram/src/anagram.cr
@@ -1,16 +1,25 @@
 class Anagram
   def self.find(str : String, arr : Array(String)) : Array(String)
     res = [] of String
-    arr.each do |s|
-      if str.size == s.size
-        x = str.chars.sort.join
-        y = s.chars.sort.join
+    str = str.upcase
+    x = str.chars.sort.join
 
-        if x == y
-          res << s
-        end
+    arr.each do |s|
+      if str.size != s.size
+        next
+      end
+      
+      if str == s.upcase
+        next
+      end
+
+      y = s.upcase.chars.sort.join
+
+      if x == y
+        res << s
       end
     end
+
     res
   end
 end
