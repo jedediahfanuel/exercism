79ba866adf3ffa846da1ffaacfd6fa397f52931d
Jeddi212 <jedediahf51@gmail.com>
2022-06-27 16:02:06 +0700

    rfc(crystal): armstrong number 🚀

--- crystal/armstrong-numbers/src/armstrong_numbers.cr
+++ crystal/armstrong-numbers/src/armstrong_numbers.cr
@@ -1,9 +1,9 @@
 class ArmstrongNumbers
-  def self.armstrong_number?(n : Int32)
+  def self.armstrong_number?(n : Int32) : Bool?
     arr = n.digits
-    size = arr.size
+    len = arr.size
     
-    res = arr.map { |d| d ** size }.sum
+    res = arr.map { |d| d ** len }.sum
 
     n == res ? true : false
   end
