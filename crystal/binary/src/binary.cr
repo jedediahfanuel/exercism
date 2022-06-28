bfd30bc8e03c4c4a1026f66b285842c4f0be54dc
Jeddi212 <jedediahf51@gmail.com>
2022-06-28 22:07:38 +0700

    rfc(binary): implement to_decimal & spec

--- crystal/binary/src/binary.cr
+++ crystal/binary/src/binary.cr
@@ -1,1 +1,7 @@
-# Please implement your solution to binary in this file
+class Binary
+  def self.to_decimal(b : String)
+    b.chars.reverse_each.map_with_index { |c, i| 
+      c == '1' ? 2 ** i : raise ArgumentError.new unless c == '0'
+    }.compact.sum
+  end
+end
