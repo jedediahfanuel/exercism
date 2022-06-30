4f23406ab057575c7a57491b572fa90d1ca35ab3
Jeddi212 <jedediahf51@gmail.com>
2022-06-30 16:13:40 +0700

    add(clock): to_s + - ⏲️

--- crystal/clock/src/clock.cr
+++ crystal/clock/src/clock.cr
@@ -1,3 +1,24 @@
 class Clock
-  # Implement the Clock class
+  property hour : Int32
+  property minute : Int32
+
+  def initialize(@hour = 0, @minute = 0) 
+
+  end
+
+  def to_s : String
+    String.build(5) do |buffer|
+      h = hour.to_s.rjust(2, '0')
+      m = minute.to_s.rjust(2, '0')
+      buffer << "#{h}:#{m}"
+    end
+  end
+
+  def + (c : Clock)
+    Clock.new(self.hour += c.hour, self.minute += c.minute)
+  end
+  
+  def - (c : Clock) : Clock
+    Clock.new(self.hour -= c.hour, self.minute -= c.minute)
+  end
 end
