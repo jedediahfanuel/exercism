a4e1218d94dd816a23e7041555f2e4b9b777b65b
Jeddi212 <jedediahf51@gmail.com>
2022-07-05 16:57:07 +0700

    add(collatz): steps function 🐉

--- crystal/collatz-conjecture/src/collatz_conjecture.cr
+++ crystal/collatz-conjecture/src/collatz_conjecture.cr
@@ -1,1 +1,9 @@
-# Please implement your solution to collatz-conjecture in this file
+class CollatzConjecture
+  def self.steps(n : Int32)
+    raise ArgumentError.new if n < 1
+    n.times do |i|
+      return i if n == 1
+      n = n.even? ? (n / 2).to_i : (3 * n + 1)
+    end
+  end
+end
