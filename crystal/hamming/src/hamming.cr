0a00f5f04e15d5d79674d8511f5cb356aa1849c0
Jeddi212 <jedediahf51@gmail.com>
2022-07-06 00:18:39 +0700

    rfc(hamming): manual distance 🚧

--- crystal/hamming/src/hamming.cr
+++ crystal/hamming/src/hamming.cr
@@ -3,6 +3,10 @@ require "levenshtein"
 class Hamming
   def self.distance(left : String, right : String) : Int32
     raise ArgumentError.new "Left.size != Right.size" if left.size != right.size
-    Levenshtein.distance(left, right)
+    count = 0_i32
+    left.size.times do |i|
+      count += 1 if left[i] != right[i]
+    end
+    count
   end
 end
