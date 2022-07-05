214680efd698abdec7f55d2cb451868175e92f20
Jeddi212 <jedediahf51@gmail.com>
2022-07-06 00:14:19 +0700

    add(hamming): levenshtein

--- crystal/hamming/src/hamming.cr
+++ crystal/hamming/src/hamming.cr
@@ -1,1 +1,8 @@
-# Please implement your solution to hamming in this file
+require "levenshtein"
+
+class Hamming
+  def self.distance(left : String, right : String) : Int32
+    raise ArgumentError.new "Left.size != Right.size" if left.size != right.size
+    Levenshtein.distance(left, right)
+  end
+end
