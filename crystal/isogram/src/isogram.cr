e78049853152bed34a84b4a94d6d4855b9a658a8
Jeddi212 <jedediahf51@gmail.com>
2022-07-06 00:43:32 +0700

    add(iso): isogram? function 🧾

--- crystal/isogram/src/isogram.cr
+++ crystal/isogram/src/isogram.cr
@@ -1,1 +1,8 @@
-# Please implement your solution to isogram in this file
+class Isogram
+  def self.isogram?(s : String)
+    arr = s.downcase.chars.reject { |c| c == ' ' || c == '-' }
+    return true if arr.size < 1
+
+    arr.size == Set.new(arr).size ? true : false
+  end
+end
