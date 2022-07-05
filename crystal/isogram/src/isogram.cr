1e039b051d6054d115c57631d67c145f65bafab7
Jeddi212 <jedediahf51@gmail.com>
2022-07-06 00:52:33 +0700

    rfc(iso): remove return 🪃

--- crystal/isogram/src/isogram.cr
+++ crystal/isogram/src/isogram.cr
@@ -1,8 +1,6 @@
 class Isogram
   def self.isogram?(s : String)
     arr = s.downcase.chars.reject { |c| c == ' ' || c == '-' }
-    return true if arr.size < 1
-
     arr.size == Set.new(arr).size ? true : false
     # arr.size == arr.uniq.size ? true : false
   end
