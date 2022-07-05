19cffe29319c851213a985b43829e87082587a18
Jeddi212 <jedediahf51@gmail.com>
2022-07-05 17:33:25 +0700

    add(ss): sum of squares 🦬

--- crystal/difference-of-squares/src/difference_of_squares.cr
+++ crystal/difference-of-squares/src/difference_of_squares.cr
@@ -1,5 +1,9 @@
 class Squares
-  def self.square_of_sum(n : Int32)
-    return (((n**2) * (n + 1)**2) / 4)
+  def self.square_of_sum(n : Int64) : Int64
+    (((n**2) * (n + 1)**2) / 4).to_i64
+  end
+
+  def self.sum_of_squares(n : Int64) : Int64
+    (n * (n + 1) * (2*n + 1) / 6).to_i64
   end
 end
