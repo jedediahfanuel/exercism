de141948268de8046c1355e71fd361a5c4d46e5e
Jeddi212 <jedediahf51@gmail.com>
2022-07-06 15:33:20 +0700

    add(lsp): finish up leap year function 📆

--- crystal/leap/src/leap.cr
+++ crystal/leap/src/leap.cr
@@ -1,5 +1,7 @@
 class Year
   def self.leap?(year : Int32) : Bool
-    true
+    return true  if year.divisible_by?(400)
+    return false if year.divisible_by?(100)
+    return year.divisible_by?(4) ? true : false
   end
 end
