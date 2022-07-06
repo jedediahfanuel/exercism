a47421c40a47541accc33791af4656598ad66d91
Jeddi212 <jedediahf51@gmail.com>
2022-07-06 15:42:53 +0700

    rfc(leap): 📆

--- crystal/leap/src/leap.cr
+++ crystal/leap/src/leap.cr
@@ -2,7 +2,7 @@ class Year
   def self.leap?(year : Int32) : Bool
     return true  if year.divisible_by?(400)
     return false if year.divisible_by?(100)
-    return year.divisible_by?(4) ? true : false
+    return          year.divisible_by?(4)
 
     # year.divisible_by?(4) && !year.divisible_by?(100) || year.divisible_by?(400)
   end
