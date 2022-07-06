61fd58123872eaed05a8282d9df7e6542c75aeec
Jeddi212 <jedediahf51@gmail.com>
2022-07-06 21:31:56 +0700

    add(rain) convert function  💧

--- crystal/raindrops/src/raindrops.cr
+++ crystal/raindrops/src/raindrops.cr
@@ -1,1 +1,9 @@
-# Please implement your solution to raindrops in this file
+class Raindrops
+  def self.convert(n : Int32) : String
+    result  = ""
+    result += "Pling" if n.divisible_by?(3)
+    result += "Plang" if n.divisible_by?(5)
+    result += "Plong" if n.divisible_by?(7)
+    result.blank? ? n.to_s : result
+  end
+end
