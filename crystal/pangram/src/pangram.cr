d5927e3bfe5a972819a35edd7a1a3968537c3dd3
Jeddi212 <jedediahf51@gmail.com>
2022-07-06 19:59:30 +0700

    add(pan): add pangram? function ✨

--- crystal/pangram/src/pangram.cr
+++ crystal/pangram/src/pangram.cr
@@ -1,1 +1,5 @@
-# Please implement your solution to pangram in this file
+class Pangram
+  def self.pangram?(s : String) : Bool
+    s.downcase.chars.select(&.ascii_letter?).tally.size == 26
+  end
+end
