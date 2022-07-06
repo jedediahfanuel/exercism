0b64557a1e81f504d52b4619a0ce1b3a9441e12b
Jeddi212 <jedediahf51@gmail.com>
2022-07-06 14:29:44 +0700

    add(lsp): largest product w/ standard int value 🔂

--- crystal/largest-series-product/src/largest_series_product.cr
+++ crystal/largest-series-product/src/largest_series_product.cr
@@ -3,10 +3,17 @@ class Series
     @value = @value.strip
   end
 
-  def largest_product(d : Int32)
+  def largest_product(digit : Int32)
     raise ArgumentError.new unless @value.scan(/\D/).empty?
-    raise ArgumentError.new if @value.size < d
-    raise ArgumentError.new if d < 0
-    return 1 if d == 0
+    raise ArgumentError.new if @value.size < digit
+    raise ArgumentError.new if digit < 0
+    return 1 if digit == 0
+
+    largest = 0
+    @value.to_i128.digits.each_cons(digit) do |series|
+      largest = series.product > largest ? series.product : largest
+    end
+
+    largest
   end
 end
