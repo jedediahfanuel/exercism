d8293b254f7b08ac0306bbe2182ab2e3970ef893
Jeddi212 <jedediahf51@gmail.com>
2022-07-06 20:47:29 +0700

    add(tri): add rows function 🔺

--- crystal/pascals-triangle/src/pascals_triangle.cr
+++ crystal/pascals-triangle/src/pascals_triangle.cr
@@ -1,5 +1,35 @@
-class PascalTriangle
-  def self.rows(r : Int32) : Array
+class PascalsTriangle
+  def self.rows(r : Int32) : Array(Array(Int32))
+    result = [] of Array(Int32)
     
+    r.times do |i|
+      row = Array(Int32).new(i+1, 0)
+
+      (0..i).each do |j|
+        if j == 0 || j == i
+          row[j] = 1
+        else
+          row[j] = result[i-1][j-1] + result[i-1][j]
+        end
+      end
+
+      result << row
+    end
+
+    result
   end
 end
+
+PascalsTriangle.rows(3).each do |x|
+  puts x
+end
+# ber = [] of Array(Int32)
+# ber.push [1]
+
+# (1..3).each do |i|
+#   rrr = Array.new(i+1, 0)
+#   rrr[i] = i + 10
+#   ber << rrr
+# end
+
+# puts ber
