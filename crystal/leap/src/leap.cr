a47a2adab2292ea535f968d6041f987d90a946ae
Jeddi212 <jedediahf51@gmail.com>
2022-07-06 15:35:33 +0700

    add(leap): 2nd logic 🗓️

--- crystal/leap/src/leap.cr
+++ crystal/leap/src/leap.cr
@@ -3,5 +3,7 @@ class Year
     return true  if year.divisible_by?(400)
     return false if year.divisible_by?(100)
     return year.divisible_by?(4) ? true : false
+
+    # year.divisible_by?(4) && !year.divisible_by?(100) || year.divisible_by?(400)
   end
 end
