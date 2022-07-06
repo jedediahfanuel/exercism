1245f5ab9593ccaca1352753ff28c3ebcb6ce7a7
Jeddi212 <jedediahf51@gmail.com>
2022-07-06 19:23:40 +0700

    add(brck): add valid function 🪣

--- crystal/matching-brackets/src/matching_brackets.cr
+++ crystal/matching-brackets/src/matching_brackets.cr
@@ -1,1 +1,26 @@
-# Please implement your solution to matching-brackets in this file
+class MatchingBrackets
+  def self.valid?(brackets : String) : Bool
+    arr = Array(Char).new
+    
+    brackets.each_char do |b|
+      case b
+      when '[', '{', '('
+        arr << b
+      when ']', '}', ')'
+        top = arr.pop?
+        return false if top == nil
+        
+        case b
+        when ']'
+          return false if top != '['
+        when '}'
+          return false if top != '{'
+        when ')'
+          return false if top != '('
+        end
+      end
+    end
+
+    arr.size < 1 ? true : false
+  end
+end
