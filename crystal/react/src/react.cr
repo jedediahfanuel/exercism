97267d60dd3297b261b9d593a0e2ad23b1bd3334
Jeddi212 <jedediahf51@gmail.com>
2022-07-07 16:30:28 +0700

    add(react): 3 spec 📑

--- crystal/react/src/react.cr
+++ crystal/react/src/react.cr
@@ -1,1 +1,24 @@
-# Please implement your solution to react in this file
+module React
+  class InputCell
+    property value : Int32
+
+    def initialize(@value : Int32)
+    end
+
+    def + (n : Int32)
+      @value += n
+    end
+  end
+
+  class ComputeCell
+    property value : React::InputCell
+
+    def initialize(@value : React::InputCell)
+      yield @value
+    end
+
+    def value
+      @value.value
+    end
+  end
+end
