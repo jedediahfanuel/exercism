50b7826add08097b0440ccaad45f08bd4d7c1549
Jeddi212 <jedediahf51@gmail.com>
2022-07-08 17:23:20 +0700

    fix(resc): missing double quotes 🫕

--- crystal/resistor-color/src/resistor_color.cr
+++ crystal/resistor-color/src/resistor_color.cr
@@ -1,7 +1,7 @@
 class ResistorColor
   @@colors : Hash(String, Int8) = {
-    "black"  => 0_i8, brown"  => 1_i8, red"    => 2_i8, orange" => 3_i8, yellow" => 4_i8,
-    "green"  => 5_i8, blue"   => 6_i8, violet" => 7_i8, grey"   => 8_i8, white"  => 9_i8
+    "black"  => 0_i8, "brown"  => 1_i8, "red"    => 2_i8, "orange" => 3_i8, "yellow" => 4_i8,
+    "green"  => 5_i8, "blue"   => 6_i8, "violet" => 7_i8, "grey"   => 8_i8, "white"  => 9_i8
   }
   
   def self.colors : Array(String)
