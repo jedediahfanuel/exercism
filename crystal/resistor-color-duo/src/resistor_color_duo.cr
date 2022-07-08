a227ca745b050fca192febe858bc47239658c087
Jeddi212 <jedediahf51@gmail.com>
2022-07-08 17:28:56 +0700

    add(rescd): add value function 🥟

--- crystal/resistor-color-duo/src/resistor_color_duo.cr
+++ crystal/resistor-color-duo/src/resistor_color_duo.cr
@@ -1,1 +1,10 @@
-# Please implement your solution to resistor-colors in this file
+class ResistorColorDuo
+  @@colors : Hash(String, Char) = {
+    "black"  => '0', "brown"  => '1', "red"    => '2', "orange" => '3', "yellow" => '4',
+    "green"  => '5', "blue"   => '6', "violet" => '7', "grey"   => '8', "white"  => '9'
+  }
+  
+  def self.value(c : Array(String)) : Int8
+    (@@colors[c[0].downcase] + "" + @@colors[c[1].downcase]).to_i8
+  end
+end
