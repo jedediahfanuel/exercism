97ba5fc85d4d6c3bdb4e407cb79bc47930554963
Jeddi212 <jedediahf51@gmail.com>
2022-07-10 20:29:33 +0700

    add(rna): of_dna function 🧬

--- crystal/rna-transcription/src/rna_transcription.cr
+++ crystal/rna-transcription/src/rna_transcription.cr
@@ -1,1 +1,10 @@
-# Please implement your solution to rna-transcription in this file
+class RnaComplement
+  @@code : Hash(Char, String) = {
+    'C' => "G", 'G' => "C", 'T' => "A", 'A' => "U"
+  }
+  
+  def self.of_dna(c : String) : String
+    c.upcase.chars.map { |x| @@code[x] }.join
+  end
+end
+
