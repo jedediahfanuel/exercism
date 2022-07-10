bad36d6ff846f7f5f1a8e23f05f1091691cd056a
Jeddi212 <jedediahf51@gmail.com>
2022-07-10 22:38:28 +0700

    add(roman): 2nd logic 🏺

--- crystal/roman-numerals/src/roman_numerals.cr
+++ crystal/roman-numerals/src/roman_numerals.cr
@@ -24,5 +24,29 @@ struct Int
     
     result
   end
+  
+  private ROMAN_DIGITS = {
+    "M" => 1000,
+    "CM" => 900,
+    "D" => 500,
+    "CD" => 400,
+    "C" => 100,
+    "XC" => 90,
+    "L" => 50,
+    "XL" => 40,
+    "X" => 10,
+    "IX" => 9,
+    "V" => 5,
+    "IV" => 4,
+    "I" => 1
+  }
+  
+  def to_roman_two
+    remainder = self
+    ROMAN_DIGITS.map do |sigil, value|
+      quotient, remainder = remainder.divmod(value)
+      sigil * quotient
+    end.join
+  end
 end
 
