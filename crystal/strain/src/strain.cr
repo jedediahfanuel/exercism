a798b3ff37a2378c38404132f402da476b3a4278
Jeddi212 <jedediahf51@gmail.com>
2022-07-14 17:20:23 +0700

    rfc(strain): undelete macro logic 🕎

--- crystal/strain/src/strain.cr
+++ crystal/strain/src/strain.cr
@@ -11,3 +11,18 @@ class Array
   end
 end
 
+class Array_two
+  @@token : Hash(String, String) = { "keep" => "keep", "discard" => "discard" }
+  
+  {% for name in %w{keep discard} %}
+    def {{name.id}}(&block : T -> Bool)
+      self.map do |x|
+        case @@token["{{name.id}}"]
+        when "keep" then x if     yield x
+        else             x unless yield x
+        end
+      end.compact
+    end
+  {% end %}
+end
+
