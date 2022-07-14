9a9c6611cbc9ecdf0529d613662224b4ac101427
Jeddi212 <jedediahf51@gmail.com>
2022-07-14 16:24:53 +0700

    add(strain): keep & discard function 🕎

--- crystal/strain/src/strain.cr
+++ crystal/strain/src/strain.cr
@@ -1,1 +1,22 @@
-# Please implement your solution to strain in this file
+class Array
+  def keep(&block : T -> Bool)
+    ary = [] of T
+    
+    self.each do |x|
+      ary << x if yield x
+    end
+    
+    ary
+  end
+  
+  def discard(&block : T -> Bool)
+    ary = [] of T
+    
+    self.each do |x|
+      ary << x unless yield x
+    end
+    
+    ary
+  end
+end
+
