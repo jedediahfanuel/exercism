e7c92ebf8e7a37d86db82938dc04e8d0a28991e2
Jeddi212 <jedediahf51@gmail.com>
2022-07-14 17:18:55 +0700

    rfc(strain): restore to unmacro 🕎

--- crystal/strain/src/strain.cr
+++ crystal/strain/src/strain.cr
@@ -1,15 +1,13 @@
 class Array
-  @@token : Hash(String, String) = { "keep" => "if", "discard" => "unless" }
-  
-  {% for name in %w{keep discard} %}
-    def {{name.id}}(&block : T -> Bool)
-      self.map do |x|
-        case @@token["{{name.id}}"]
-        when "if" then x if yield x
-        else           x unless yield x
-        end
-      end.compact
-    end
-  {% end %}
+  def keep(&block : T -> Bool)
+    self.map do |x|
+      x if yield x
+    end.compact
+  end
+  def discard(&block : T -> Bool)
+    self.map do |x|
+      x unless yield x
+    end.compact
+  end
 end
 
