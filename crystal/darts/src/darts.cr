b91cd70c402fff61fd23adf58e5fd3accbf4d68b
Jeddi212 <jedediahf51@gmail.com>
2022-07-14 13:22:44 +0700

    add(dart): score function 🎯

--- crystal/darts/src/darts.cr
+++ crystal/darts/src/darts.cr
@@ -1,1 +1,11 @@
-# Please implement your solution to darts in this file
+class Darts
+  def self.score(x : Float32, y : Float32) : Int8
+    case Math.hypot(x, y)
+    when .<= 1  then 10_i8
+    when .<= 5  then 5_i8
+    when .<= 10 then 1_i8
+    else             0_i8
+    end
+  end
+end
+
