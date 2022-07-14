7882a6bcdc6337b78c90b0c034b541e30efcdd33
Jeddi212 <jedediahf51@gmail.com>
2022-07-14 12:20:58 +0700

    rfc(tri): minor code clean-up 🔻

--- crystal/triangle/src/triangle.cr
+++ crystal/triangle/src/triangle.cr
@@ -6,10 +6,9 @@ class Triangle
   end
   
   def isInvalid(a, b, c) : Bool
-    return true if a == b && b == c && c == 0
     case {((a + b) > c), ((b + c) > a), ((c + a) > b)}
-    when {true, true, true} then return false
-    else                         return true
+    when {true, true, true} then false
+    else                         true
     end
   end
   
@@ -26,15 +25,15 @@ class Triangle
   end
   
   def isosceles_two?
-      @sides.to_set.size >= 1 && !scalene?
+    @sides.to_set.size >= 1 && !scalene?
   end
   
   def scalene_two?
-      @sides.to_set.size == 3
+    @sides.to_set.size == 3
   end
   
   def inequality_two?
-      x = @sides.to_a.sort
-      x[0] + x[1] < x[2]
-  end
+    x = @sides.to_a.sort
+    x[0] + x[1] < x[2]
+  end  
 end
