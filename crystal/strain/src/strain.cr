80d5e2aa4b93c00550f48ff42d6ecbd29f254351
Jeddi212 <jedediahf51@gmail.com>
2022-07-14 17:27:29 +0700

    rfc(strain): minor enchancement 🕎

--- crystal/strain/src/strain.cr
+++ crystal/strain/src/strain.cr
@@ -1,13 +1,9 @@
 class Array
   def keep(&block : T -> Bool)
-    self.map do |x|
-      x if yield x
-    end.compact
+    self.map { |x| x if yield x }.compact
   end
   def discard(&block : T -> Bool)
-    self.map do |x|
-      x unless yield x
-    end.compact
+    self.map { |x| x unless yield x }.compact
   end
 end
 
