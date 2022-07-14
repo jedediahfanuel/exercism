6835bef960577903a46e782c81531722859422d3
Jeddi212 <jedediahf51@gmail.com>
2022-07-14 14:40:11 +0700

    add(isbn): valid function 📙

--- crystal/isbn-verifier/src/isbn_verifier.cr
+++ crystal/isbn-verifier/src/isbn_verifier.cr
@@ -1,1 +1,20 @@
-# Please implement your solution to isbn-verifier in this file
+class IsbnVerifier
+  def self.valid?(isbn : String)
+    isbn = isbn.gsub("-", "").chars.reverse
+    return false if isbn.size != 10
+    
+    isbn.map_with_index do |c, i|
+      i += 1
+      if !c.ascii_number?
+        if c == 'X' && i == 1
+          10 * i
+        else
+          return false
+        end
+      else
+        c.to_i * i
+      end
+    end.sum.divisible_by? 11
+  end
+end
+
