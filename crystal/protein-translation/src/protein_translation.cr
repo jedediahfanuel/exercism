bfedd00c9cf81ac0475e769938238cb2c4ae0e81
Jeddi212 <jedediahf51@gmail.com>
2022-07-15 20:52:38 +0700

    add(pt): proteins function 🧬

--- crystal/protein-translation/src/protein_translation.cr
+++ crystal/protein-translation/src/protein_translation.cr
@@ -1,1 +1,18 @@
-# Please implement your solution to protein-translation in this file
+class ProteinTranslation
+  def self.proteins(s : String)
+    r = [] of String
+    s.scan(/.{3}/).map(&.[0]).each do |x|
+      case x
+      when "AUG"                      then r << "Methionine"
+      when "UUU", "UUC"               then r << "Phenylalanine"
+      when "UUA", "UUG"               then r << "Leucine"
+      when "UCU", "UCC", "UCA", "UCG" then r << "Serine"
+      when "UAU", "UAC"               then r << "Tyrosine"
+      when "UGU", "UGC"               then r << "Cysteine"
+      when "UGG"                      then r << "Tryptophan"
+      else break
+      end
+    end ; r
+  end
+end
+
