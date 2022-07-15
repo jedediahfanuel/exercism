ab5e07dd8b3d3ee11f5a3394e8367fa3b6611937
Jeddi212 <jedediahf51@gmail.com>
2022-07-15 13:46:03 +0700

    add(base): functionality 🔢

--- crystal/all-your-base/src/all_your_base.cr
+++ crystal/all-your-base/src/all_your_base.cr
@@ -1,1 +1,30 @@
-# Please implement your solution to all-your-base in this file
+class AllYourBase
+  def self.rebase(i : Int32, seq : Array(Int32), o : Int32) : Array(Int32)
+    raise ArgumentError.new if i < 2 || o < 2
+    return [0] unless seq.any?
+    
+    to_output_base( to_base_ten( seq , i ) , o )
+  end
+  
+  private def self.to_base_ten(seq : Array(Int32), b : Int32) : Int32
+    seq.reverse.map_with_index do |d, i|  
+      if d < 0 || d >= b
+        raise ArgumentError.new "Invalid input digit."
+      end
+      
+      d * (b ** i)
+    end.sum
+  end
+  
+  private def self.to_output_base(ten_base_num : Int32, b : Int32) : Array(Int32)
+    res = [] of Int32
+
+    while ten_base_num >= b
+      res << ten_base_num % b
+      ten_base_num //= b
+    end
+
+    res << ten_base_num % b
+    res.reverse
+  end
+end
