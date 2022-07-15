00f4ff4d377c4d494b08259fe30170b3f78af035
Jeddi212 <jedediahf51@gmail.com>
2022-07-15 17:58:50 +0700

    add(nth): primes function 🛻

--- crystal/nth-prime/src/nth_prime.cr
+++ crystal/nth-prime/src/nth_prime.cr
@@ -1,1 +1,19 @@
-# Please implement your solution to nth-prime in this file
+class NthPrime
+  def self.prime(n : Int32) : Int32
+    raise ArgumentError.new if n < 1
+    primes = [2]
+    loop do
+      break primes.last unless primes.size < n
+      primes << next_prime(primes)
+    end
+  end
+  
+  def self.next_prime(primes : Array(Int32)) : Int32
+    n = primes.last + 1
+    loop do
+      break n if primes.none? { |x| (n).divisible_by? x }
+      n += 1
+    end
+  end
+end
+
