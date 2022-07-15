1b2eeae34910dc8133b6349c13d996cee1c281c9
Jeddi212 <jedediahf51@gmail.com>
2022-07-15 19:50:20 +0700

    add(pf): factors function 🌲

--- crystal/prime-factors/src/prime_factors.cr
+++ crystal/prime-factors/src/prime_factors.cr
@@ -1,1 +1,21 @@
-# Please implement your solution to prime-factors in this file
+class PrimeFactors
+  def self.factors(n : Int64) : Array(Int64)
+    res = [] of Int64
+    while n.divisible_by? 2
+      res << 2
+      n //= 2
+    end
+    
+    (3..Math.sqrt(n)+1).step(by: 2).each do |i|
+      while n.divisible_by? i
+        res << i
+        n //= i
+      end
+    end
+    
+    res << n if n > 2
+    res
+  end
+end
+
+# https://www.geeksforgeeks.org/print-all-prime-factors-of-a-given-number/
