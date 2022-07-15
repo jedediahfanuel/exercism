0fcaced31e123029e53b2357468e35f8954d73cd
Jeddi212 <jedediahf51@gmail.com>
2022-07-15 23:27:05 +0700

    add(gr): square & total function 🌽

--- crystal/grains/src/grains.cr
+++ crystal/grains/src/grains.cr
@@ -1,1 +1,10 @@
-# Please implement your solution to grains in this file
+class Grains
+  def self.square(n : Int32) : UInt128
+    raise ArgumentError.new if n < 1 || n > 64
+    2.to_u128**(n - 1)
+  end
+  
+  def self.total : UInt128
+    2.to_u128**64 - 1
+  end
+end
