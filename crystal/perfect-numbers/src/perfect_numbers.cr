fe7194cb59b43cea63599790c36b8cbd2c5bafcb
Jeddi212 <jedediahf51@gmail.com>
2022-07-18 15:40:16 +0700

    add(perf): classifiy & aliquot function 🎱

--- crystal/perfect-numbers/src/perfect_numbers.cr
+++ crystal/perfect-numbers/src/perfect_numbers.cr
@@ -1,1 +1,14 @@
-# Please implement your solution to perfect-numbers in this file
+class PerfectNumbers
+  def self.classify(n : Int32) : String
+    raise ArgumentError.new if n < 1
+    
+    return "abundant"  if aliquot_sum(n) > n
+    return "deficient" if aliquot_sum(n) < n
+    return "perfect"
+  end
+  
+  def self.aliquot_sum(n : Int32) : Int32
+    (1..(n // 2)).map { |i| n.divisible_by?(i) ? i : nil }.compact.sum
+  end
+end
+
