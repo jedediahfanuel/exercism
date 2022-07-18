d17ad4716de565b5c5c960955aa92f98fb1ac2f4
Jeddi212 <jedediahf51@gmail.com>
2022-07-18 15:45:17 +0700

    rfc(perf): classifiy & aliquot function 🎱

--- crystal/perfect-numbers/src/perfect_numbers.cr
+++ crystal/perfect-numbers/src/perfect_numbers.cr
@@ -1,14 +1,8 @@
 class PerfectNumbers
   def self.classify(n : Int32) : String
     raise ArgumentError.new if n < 1
-    
-    return "abundant"  if aliquot_sum(n) > n
-    return "deficient" if aliquot_sum(n) < n
-    return "perfect"
-  end
-  
-  def self.aliquot_sum(n : Int32) : Int32
-    (1..(n // 2)).map { |i| n.divisible_by?(i) ? i : nil }.compact.sum
+    alsum = (1..(n // 2)).map { |i| n.divisible_by?(i) ? i : nil }.compact.sum
+    alsum == n ? "perfect" : alsum < n ? "deficient" : "abundant"
   end
 end
 
