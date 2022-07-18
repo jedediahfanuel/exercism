679eff55e0f97af40e19bf78b7a68d5720a9b4af
Jeddi212 <jedediahf51@gmail.com>
2022-07-18 13:51:02 +0700

    add(lll): preparation_time_in_minutes 🍪

--- crystal/lucians-luscious-lasagna/src/lucians_luscious_lasagna.cr
+++ crystal/lucians-luscious-lasagna/src/lucians_luscious_lasagna.cr
@@ -12,4 +12,8 @@ class LuciansLusciousLasagna
     # Each layer of lasagna assumed to be 2 minutes preparation time
     number_of_layers * 2
   end
+  
+  def self.elapsed_time_in_minutes(number_of_layers: Int32, actual_minutes_in_oven: Int32) : Int32
+    preparation_time_in_minutes(number_of_layers) + actual_minutes_in_oven
+  end
 end
