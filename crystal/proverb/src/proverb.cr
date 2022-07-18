ae04d4aee7a2d6ac302a7db17328963f308cc2be
Jeddi212 <jedediahf51@gmail.com>
2022-07-18 16:20:45 +0700

    add(prov): recite function 📊

--- crystal/proverb/src/proverb.cr
+++ crystal/proverb/src/proverb.cr
@@ -1,1 +1,14 @@
-# Please implement your solution to proverb in this file
+class Proverb
+  def self.recite(term : Array(String)) : Array(String)
+    res = [] of String
+    
+    (1...term.size).each do |i|
+      res << "For want of a #{term[i-1]} the #{term[i]} was lost."
+    end
+    
+    res << "And all for the want of a #{term[0]}." if term.any?
+    
+    res
+  end
+end
+
