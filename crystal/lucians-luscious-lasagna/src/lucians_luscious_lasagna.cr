d684f37fff87180161249519b018f2c2f86ce322
Jeddi212 <jedediahf51@gmail.com>
2022-07-18 13:47:13 +0700

    add(lll): remaining_minutes_in_oven 🍪

--- crystal/lucians-luscious-lasagna/src/lucians_luscious_lasagna.cr
+++ crystal/lucians-luscious-lasagna/src/lucians_luscious_lasagna.cr
@@ -3,4 +3,8 @@ class LuciansLusciousLasagna
     # According to the coocking book, the exepected oven time in minutes is 40
     40
   end
+  
+  def self.remaining_minutes_in_oven(actual_minutes_in_oven : Int32) : Int32
+    40 - actual_minutes_in_oven
+  end
 end
