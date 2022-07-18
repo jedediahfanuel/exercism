e6a4441fd05ca1214c315f3e1820b5a0bf1fa45f
Jeddi212 <jedediahf51@gmail.com>
2022-07-18 15:48:02 +0700

    rfc(perf): classifiy & aliquot function 🎱

--- crystal/perfect-numbers/src/perfect_numbers.cr
+++ crystal/perfect-numbers/src/perfect_numbers.cr
@@ -1,7 +1,7 @@
 class PerfectNumbers
   def self.classify(n : Int32) : String
     raise ArgumentError.new if n < 1
-    alsum = (1..(n // 2)).map { |i| n.divisible_by?(i) ? i : nil }.compact.sum
+    alsum = (1..(n // 2)).sum { |i| n.divisible_by?(i) ? i : 0 }
     alsum == n ? "perfect" : alsum < n ? "deficient" : "abundant"
   end
 end
