6c95f59aef0146bbe227b3dc6b3ab4563e62d094
Jeddi212 <jedediahf51@gmail.com>
2022-07-18 17:31:27 +0700

    add(sh): 3rd commands 🚇

--- crystal/secret-handshake/src/secret_handshake.cr
+++ crystal/secret-handshake/src/secret_handshake.cr
@@ -31,5 +31,15 @@ module SecHand
     handshake.reverse! if input.bits_set?(REVERSE)
     handshake
   end
+  
+  def self.commands_three(n : Int) : Array(String)
+    out = [] of String
+    out << "wink"            if n.bit(0) == 1
+    out << "double blink"    if n.bit(1) == 1
+    out << "close your eyes" if n.bit(2) == 1
+    out << "jump"            if n.bit(3) == 1
+    out.reverse!             if n.bit(4) == 1
+    out
+  end
 end
 
