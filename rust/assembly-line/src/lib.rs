9e49593f23e628450930822008d977f9244f90d5
Jeddi212 <jedediahf51@gmail.com>
2022-07-18 12:34:45 +0700

    add(assm): 2nd production rate per hour 💹

--- rust/assembly-line/src/lib.rs
+++ rust/assembly-line/src/lib.rs
@@ -19,3 +19,15 @@ pub fn production_rate_per_hour(speed: u8) -> f64 {
 pub fn working_items_per_minute(speed: u8) -> u32 {
     production_rate_per_hour(speed) as u32 / 60
 }
+
+////////////////////////////////////////////////
+
+pub fn production_rate_per_hour(speed: u8) -> f64 {
+    match speed {
+        0 => 0.0,
+        s @ 1..=4 => PRODUCTION_RATE_DEFAULT * s as f64,
+        s @ 5..=8 => PRODUCTION_RATE_DEFAULT * s as f64 * 0.9,
+        s @ 9..=10 => PRODUCTION_RATE_DEFAULT * s as f64 * 0.77,
+        _ => panic!("Speed should be a value from 0 to 10"),
+    }
+}
