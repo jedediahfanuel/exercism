65f4345e66034072057a828f6e8ddc4673d701c1
Jeddi212 <jedediahf51@gmail.com>
2022-07-18 16:23:11 +0700

    add(prov): 2nd logic 📊

--- crystal/proverb/src/proverb.cr
+++ crystal/proverb/src/proverb.cr
@@ -10,5 +10,12 @@ class Proverb
     
     res
   end
+  
+  def self.recite_two(wants)
+    return wants if wants.empty?
+    wants.each.cons_pair.map do |(a, b)|
+      "For want of a #{a} the #{b} was lost."
+    end.to_a << "And all for the want of a #{wants[0]}."
+  end  
 end
 
