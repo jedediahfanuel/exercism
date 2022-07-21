bfae148dc2063e0e1d8488f9e522c74909d7a505
Jeddi212 <jedediahf51@gmail.com>
2022-07-21 17:47:23 +0700

    add(hs): setter 🎁

--- rust/health-statistics/tests/health-statistics.rs
+++ rust/health-statistics/tests/health-statistics.rs
@@ -23,7 +23,6 @@ fn test_weight() {
 }
 
 #[test]
-#[ignore]
 fn test_set_age() {
     let new_age: u32 = 90;
     let mut user = User::new(NAME.into(), AGE, WEIGHT);
@@ -32,7 +31,6 @@ fn test_set_age() {
 }
 
 #[test]
-#[ignore]
 fn test_set_weight() {
     let new_weight: f32 = 129.4;
     let mut user = User::new(NAME.into(), AGE, WEIGHT);
