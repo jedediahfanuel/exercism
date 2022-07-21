4daa5712c7136f15352113e63b414976dc9c4616
Jeddi212 <jedediahf51@gmail.com>
2022-07-21 18:26:12 +0700

    add(ham): compute function 🧬

--- lua/hamming/hamming.lua
+++ lua/hamming/hamming.lua
@@ -1,7 +1,14 @@
 local Hamming = {}
 
 function Hamming.compute(a,b)
-
+  if #a ~= #b then return -1 end
+  local count = 0
+  for i=1, #a do
+    if (a:sub(i,i) ~= b:sub(i,i)) then
+      count = count + 1
+    end
+  end
+  return count
 end
 
 return Hamming
