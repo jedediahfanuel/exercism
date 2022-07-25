fe1a0aba54ec63ef1584cabfa018e1d8d26a76c5
Jeddi212 <jedediahf51@gmail.com>
2022-07-25 21:05:09 +0700

    add(rc): using enum 🗜️

--- crystal/resistor-color/spec/resistor_color_spec.cr
+++ crystal/resistor-color/spec/resistor_color_spec.cr
@@ -18,3 +18,39 @@ describe "ResistorColor" do
     ResistorColor.colors.should eq(["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"])
   end
 end
+
+describe "ResistorColorEnum" do
+  it "Test Black" do
+    ResistorColor.color_to_value(ResistorColor::Clr::Black).should eq(0)
+  end
+
+  it "Test White" do
+    ResistorColor.color_to_value(ResistorColor::Clr::White).should eq(9)
+  end
+
+  it "Test Orange" do
+    ResistorColor.color_to_value(ResistorColor::Clr::Orange).should eq(3)
+  end
+  
+  it "Test 2" do
+    ResistorColor.value_to_color_string(2).should eq("Red")
+  end
+
+  it "Test 6" do
+    ResistorColor.value_to_color_string(6).should eq("Blue")
+  end
+
+  it "Test 8" do
+    ResistorColor.value_to_color_string(8).should eq("Grey")
+  end
+  
+  it "Test 11 out of range" do
+    expect_raises(Exception) do
+      ResistorColor.value_to_color_string(11)
+    end
+  end
+  
+  it "Test all colors" do
+    ResistorColor.clrs().should eq(["Black", "Brown", "Red", "Orange", "Yellow", "Green", "Blue", "Violet", "Grey", "White"])
+  end
+end
--- crystal/resistor-color/src/resistor_color.cr
+++ crystal/resistor-color/src/resistor_color.cr
@@ -1,4 +1,4 @@
-class ResistorColor
+module ResistorColor
   @@colors : Hash(String, Int8) = {
     "black"  => 0_i8, "brown"  => 1_i8, "red"    => 2_i8, "orange" => 3_i8, "yellow" => 4_i8,
     "green"  => 5_i8, "blue"   => 6_i8, "violet" => 7_i8, "grey"   => 8_i8, "white"  => 9_i8
@@ -10,5 +10,31 @@ class ResistorColor
   
   def self.color_code(c : String) : Int8
     @@colors[c.downcase]
+  end
+  
+  # Using Enum
+  enum Clr
+    Black
+    Brown
+    Red
+    Orange
+    Yellow
+    Green
+    Blue
+    Violet
+    Grey
+    White
+  end
+  
+  def self.color_to_value(c : ResistorColor::Clr) : Int32
+    c.value
+  end
+  
+  def self.value_to_color_string(n : Int32) : String
+    Clr.from_value(n).to_s
+  end
+  
+  def self.clrs : Array(String)
+    Clr.names
   end
 end
