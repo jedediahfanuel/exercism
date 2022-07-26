86c22d78f63a45160707ff8f051c05630c8805dc
Jeddi212 <jedediahf51@gmail.com>
2022-07-26 21:02:02 +0700

    add(arydiff): 2nd logic simpler 🏓

--- crystal/array-diff/src/array_diff.cr
+++ crystal/array-diff/src/array_diff.cr
@@ -1,6 +1,9 @@
 class ArrayDiff
   def self.diff(a : Array(Int32), b : Array(Int32)) : Array(Int32)
-    return a if a.empty? || b.empty?
     a.reject { |x| b.any?(&.== x) }
+  end
+  
+  def self.diff_two(a : Array(Int32), b : Array(Int32)) : Array(Int32)
+    a - b
   end
 end
