85f215a0f6505a5c504ad8e2ac7078b1f37c3367
Jeddi212 <jedediahf51@gmail.com>
2022-07-26 18:14:53 +0700

    add(spst): 2nd logic 🪓

--- crystal/split-strings/src/split_strings.cr
+++ crystal/split-strings/src/split_strings.cr
@@ -3,4 +3,8 @@ class SplitStrings
     s += '_' if s.size.odd?
     s.each_char.slice(2).to_a.map(&.join)
   end
+  
+  def self.split_two(s : String)
+    s.chars.in_groups_of(2, '_').map(&.join)
+  end
 end
