327003aca8cfed9a6bf609221abc1c3ea1a6fb2c
Jeddi212 <jedediahf51@gmail.com>
2022-07-26 18:50:53 +0700

    add(dvt): 2nd 3rd logic 🎋

--- crystal/disemvowel-trolls/src/disemvowel_trolls.cr
+++ crystal/disemvowel-trolls/src/disemvowel_trolls.cr
@@ -1,5 +1,10 @@
 class Troll
   def self.disemvowel(s : String) : String
-      s.gsub(/[aeiuoAEIUO]/, "")
+    s.gsub(/[aeiuoAEIUO]/, "")
+    # gsub(/[aeiou]/i, "")
+  end
+  
+  def self.disemvowel_(str)
+    str.delete("aeiouAEIOU")
   end
 end
