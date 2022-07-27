6eaaca7273687213c0298ff654a0939b0bb8cede
Jeddi212 <jedediahf51@gmail.com>
2022-07-27 20:55:09 +0700

    add(tr): 2nd logic 🐢

--- crystal/tortoise-racing/src/tortoise_racing.cr
+++ crystal/tortoise-racing/src/tortoise_racing.cr
@@ -1,8 +1,14 @@
 class Tortoise
-  def self.race(v1, v2, g)
+  def self.race(v1, v2, g) : Array(Int32) | Nil
     return [-1, -1, -1] if v1 >= v2
     s = (g * 3600) / (v2 - v1)
     [(s // 3600).to_i, ((s % 3600) // 60).to_i, (s % 60).to_i]
+  end
+  
+  def self.race_two(v1, v2, g)
+    return if v1 > v2
+    time = (g.to_f / (v2 - v1)).hours
+    [time.hours, time.minutes, time.seconds]
   end
 end
 
