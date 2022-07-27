02e19f9bf7a3f46fc8770ffc931b30efd509d47c
Jeddi212 <jedediahf51@gmail.com>
2022-07-27 22:20:00 +0700

    add(sop): 2nd logic ♟️

--- crystal/sum-of-parts/src/sum_of_parts.cr
+++ crystal/sum-of-parts/src/sum_of_parts.cr
@@ -6,4 +6,10 @@ class Sums
     ls.each { |i| acc += i ; ary << all - acc }
     ary
   end
+  
+  def self.parts_sums_two(arr : Array(Int32|Int64))
+    result = [arr.sum]
+    arr.each { |item| result << result.last-item }
+    result
+  end
 end
