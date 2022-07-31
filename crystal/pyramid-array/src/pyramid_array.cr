a333b567d06a273721a67886d0837e56eb3c105c
Jeddi212 <jedediahf51@gmail.com>
2022-07-31 19:53:04 +0700

    add(pyarr): 2nd 3rd logic ◀️

--- crystal/pyramid-array/src/pyramid_array.cr
+++ crystal/pyramid-array/src/pyramid_array.cr
@@ -1,3 +1,11 @@
 def pyramid(n : Int32) : Array(Array(Int32))
   (1..n).map { |i| i.times.map { |_| 1 }.to_a }
 end
+
+def pyramid_two(n)
+  (1..n).map {|a| [1] * a}
+end
+
+def pyramid_three(n)
+  (1..n).map { |i| Array.new(i) { 1 } }
+end
