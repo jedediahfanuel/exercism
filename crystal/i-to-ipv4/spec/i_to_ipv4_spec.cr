967996ef594fd20e5aa38b600353459ff4594e9d
Jeddi212 <jedediahf51@gmail.com>
2022-07-31 21:10:53 +0700

    rfc(itiv4): switch content, simplify to_binary 🗼

--- crystal/i-to-ipv4/spec/i_to_ipv4_spec.cr
+++ crystal/i-to-ipv4/spec/i_to_ipv4_spec.cr
@@ -1,15 +1,10 @@
-def int32_to_ip(i : UInt32) : String
-  to_binary(i).chars.each_slice(8).map do |x|
-    x.join.to_u32(2)
-  end.join(".")
-end
+require "spec"
+require "../src/*"
 
-def to_binary(ten_base_num : UInt32)
-  res = [] of UInt32
-  while ten_base_num > 0
-    res.unshift ten_base_num % 2
-    ten_base_num //= 2
+describe "Fixed Tests" do
+  it "works" do
+    int32_to_ip(2154959208).should eq "128.114.17.104"
+    int32_to_ip(0).should eq "0.0.0.0"
+    int32_to_ip(2149583361).should eq "128.32.10.1"
   end
-  res.empty? ? [0] : res
-  res.join.rjust(32, '0')
 end
--- crystal/i-to-ipv4/src/i_to_ipv4.cr
+++ crystal/i-to-ipv4/src/i_to_ipv4.cr
@@ -1,11 +1,5 @@
-require "spec"
-require "../src/*"
-
-describe "Fixed Tests" do
-  it "works" do
-    int32_to_ip(2154959208).should eq "128.114.17.104"
-    int32_to_ip(0).should eq "0.0.0.0"
-    int32_to_ip(2149583361).should eq "128.32.10.1"
-  end
+def int32_to_ip(i : UInt32) : String
+  i.to_s(2).rjust(32, '0').chars.each_slice(8).map do |x|
+    x.join.to_u32(2)
+  end.join(".")
 end
-
