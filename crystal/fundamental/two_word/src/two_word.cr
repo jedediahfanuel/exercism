165a16d3f01e5f26f6a90baed3aaae4bdfae4472
Jeddi212 <jedediahf51@gmail.com>
2022-08-06 17:38:54 +0700

    add(fund): 2nd logic 2️

--- crystal/fundamental/two_word/src/two_word.cr
+++ crystal/fundamental/two_word/src/two_word.cr
@@ -1,3 +1,10 @@
 def abbrev_name(name)
   name.split.map { |word| word.char_at(0).upcase }.join(".")
 end
+
+def abbrev_name_two(name)
+  first, last = name.upcase.split
+  "#{first[0]}.#{last[0]}"
+end
+
+
