3d7c1d6b70eca97ec867f2ed596d4ca6503d4e7d
Jeddi212 <jedediahf51@gmail.com>
2022-08-06 16:46:57 +0700

    add(fund): perfect square ✅

--- crystal/fundamental/youre_a_square/src/youre_a_square.cr
+++ crystal/fundamental/youre_a_square/src/youre_a_square.cr
@@ -1,6 +1,11 @@
 class Shape
   def self.is_square(x) : Bool
-    y = Math.sqrt(x)
+    return false if x.negative?
+    y = Math.sqrt(x).to_i
     y * y == x ? true : false
+  end
+  
+  def self.is_square_two(x)
+    Math.sqrt(x) == Math.sqrt(x).round
   end
 end
