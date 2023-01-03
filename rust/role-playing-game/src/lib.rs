c6e8d8cf304df31a001b203b0545e34ed11500d7
Jeddi212 <jedediahf51@gmail.com>
2023-01-03 19:38:40 +0700

    ❤️‍🩹 :: revive method

--- rust/role-playing-game/src/lib.rs
+++ rust/role-playing-game/src/lib.rs
@@ -5,11 +5,17 @@ pub struct Player {
 }
 
 impl Player {
-    pub fn revive(&self) -> Option<Player> {
-        
+    pub fn revive(&self) -> Player {
+        let mut mana = None;
+        if self.level > 9 {
+            mana = Some(100);
+        }
+        Player {
+            health: 100,
+            mana,
+            level: self.level,
+        }
     }
 
-    pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
-        
-    }
+    pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {}
 }
