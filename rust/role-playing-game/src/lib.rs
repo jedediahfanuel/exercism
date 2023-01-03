79dc2bdc42a4ac52ed08a9375a32cf467dc4fc83
Jeddi212 <jedediahf51@gmail.com>
2023-01-03 19:43:02 +0700

    🪤 :: refactor to option return type

--- rust/role-playing-game/src/lib.rs
+++ rust/role-playing-game/src/lib.rs
@@ -5,16 +5,22 @@ pub struct Player {
 }
 
 impl Player {
-    pub fn revive(&self) -> Player {
+    pub fn revive(&self) -> Option<Player> {
+        if self.health > 0 {
+            return None;
+        }
+
         let mut mana = None;
+
         if self.level > 9 {
             mana = Some(100);
         }
-        Player {
+
+        Some(Player {
             health: 100,
             mana,
             level: self.level,
-        }
+        })
     }
 
     pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {}
