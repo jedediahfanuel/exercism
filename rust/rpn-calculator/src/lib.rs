1a7f07883ecbc7889c297443f63dc87c353b0f87
Jeddi212 <jedediahf51@gmail.com>
2023-01-04 00:13:34 +0700

    🍮 :: stack RPN

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -12,12 +12,32 @@ pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
         return None;
     }
 
-    if inputs.len() == 1 {
-        return match inputs[0] {
-            CalculatorInput::Value(n) => Some(n),
-            _ => None,
-        };
+    let mut stack: Vec<i32> = Vec::new();
+    for input in inputs {
+        match input {
+            CalculatorInput::Value(n) => stack.push(*n),
+            _ => {
+                if stack.len() < 2 {
+                    return None;
+                }
+
+                let n2 = stack.pop().unwrap();
+                let n1 = stack.pop().unwrap();
+
+                match input {
+                    CalculatorInput::Add => stack.push(n1 + n2),
+                    CalculatorInput::Subtract => stack.push(n1 - n2),
+                    CalculatorInput::Multiply => stack.push(n1 * n2),
+                    CalculatorInput::Divide => stack.push(n1 / n2),
+                    _ => return None,
+                }
+            }
+        }
     }
 
-    None
+    if stack.len() != 1 {
+        return None;
+    }
+
+    stack.pop()
 }
