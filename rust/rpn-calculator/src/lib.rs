19ce34be44575889f0191d5087ba915af09f7442
Jeddi212 <jedediahf51@gmail.com>
2023-01-03 23:39:28 +0700

    🧻 :: some special case

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -7,4 +7,17 @@ pub enum CalculatorInput {
     Value(i32),
 }
 
-pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {}
+pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
+    if inputs.len() < 1 {
+        return None;
+    }
+
+    if inputs.len() == 1 {
+        return match inputs[0] {
+            CalculatorInput::Value(n) => Some(n),
+            _ => None,
+        };
+    }
+
+    None
+}
