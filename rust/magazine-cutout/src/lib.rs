fe70634a8cf62499d0fe8cb598f0b822a6a9c097
Jeddi212 <jedediahf51@gmail.com>
2023-01-08 18:52:31 +0700

    👊 -- remove unimplemented macro

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -1,5 +1,3 @@
 use std::collections::HashMap;
 
-pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    unimplemented!()
-}
+pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {}
