68387ca4307be4ef2acb52483fa16061f47f5cad
Jeddi212 <jedediahf51@gmail.com>
2023-04-05 16:13:36 +0700

    🫒 :: binary search

--- crystal/binary-search/src/binary_search.cr
+++ crystal/binary-search/src/binary_search.cr
@@ -1,3 +1,18 @@
 class BinarySearch
-  # Write your code for the 'Binary Search' exercise in this file.
+  def initialize(@arr : Array(Int32)) end
+  
+  def find(v : Int32)
+    raise ArgumentError.new if @arr.empty? || v < @arr.first || v > @arr.last
+
+    x, y = 0, @arr.size-1
+
+    while x <= y
+      m = x + (y - x) // 2
+      return m if @arr[m] == v
+      y = m - 1 if v < @arr[m]
+      x = m + 1 if v > @arr[m]
+    end
+
+    raise ArgumentError.new
+  end
 end
