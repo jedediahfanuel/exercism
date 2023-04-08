60de0a4cdef14d0347e385523154d5ea7341d2f9
Jeddi212 <jedediahf51@gmail.com>
2023-04-08 19:41:31 +0700

    🍧 :: high scores done

--- crystal/high-scores/src/high_scores.cr
+++ crystal/high-scores/src/high_scores.cr
@@ -1,3 +1,7 @@
 class HighScores
-  # Write your code for the 'HighScores' exercise in this file.
+  property scores : Array(Int32)
+  def initialize(@scores : Array(Int32)) end
+  def latest             ; scores.last                 ; end
+  def personal_best      ; scores.max                  ; end
+  def personal_top_three ; scores.sort.reverse.first 3 ; end
 end
