ff01c578190a906e9b11c3f4e08a7f881f5d7004
Jeddi212 <jedediahf51@gmail.com>
2023-04-08 20:16:06 +0700

    📓 :: update lsp

--- crystal/largest-series-product/src/largest_series_product.cr
+++ crystal/largest-series-product/src/largest_series_product.cr
@@ -1,23 +1,13 @@
-class Series
+class LargestSeriesProduct
   def initialize(@value : String)
+    raise ArgumentError.new unless @value.scan(/\D/).empty?
     @value = @value.strip
   end
 
   def largest_product(digit : Int32)
-    raise ArgumentError.new unless @value.scan(/\D/).empty?
-    raise ArgumentError.new if @value.size < digit
-    raise ArgumentError.new if digit < 0
+    raise ArgumentError.new if @value.size < digit || digit < 0
     return 1 if digit == 0
 
-    largest = 0
-    @value.chars.each_cons(digit) do |series|
-      p = series.product(&.to_u64)
-      largest = p > largest ? p : largest
-    end
-
-    largest
-
-    # @value.each_char.cons(digits).max_of(&.product(&.to_u64))
-    # @value.each_cons(digit).map(&.product 1_i64).max
+    @value.each_char.cons(digit).max_of(&.product(&.to_u64))
   end
 end
