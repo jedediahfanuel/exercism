89f07bbf2b4418367fd8837f8b7bff4a1bcdb3a2
Jeddi212 <jedediahf51@gmail.com>
2023-04-08 23:49:33 +0700

    🍗 :: squeeze line

--- crystal/sublist/src/sublist.cr
+++ crystal/sublist/src/sublist.cr
@@ -5,6 +5,4 @@ module Sublist
     return Classification::Equal     if x == y
     return Classification::Sublist   if x.size < y.size && y.join(" ").matches? Regex.new x.join " "
     return Classification::Superlist if x.size > y.size && x.join(" ").matches? Regex.new y.join " "
-    return Classification::Unequal 
-  end
-end
+    return Classification::Unequal  end end
