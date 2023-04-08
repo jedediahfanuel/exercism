8eede8c84a21db79d2b99e6ce306095bae6fa94f
Jeddi212 <jedediahf51@gmail.com>
2023-04-08 22:38:35 +0700

    🛖 :: update rle

--- crystal/run-length-encoding/src/run_length_encoding.cr
+++ crystal/run-length-encoding/src/run_length_encoding.cr
@@ -1,23 +1,10 @@
 class RunLengthEncoding
   def self.encode(s : String) : String
-    last_count, last_char, result = 0, '.', ""
-
-    s.each_char do |c|
-      if last_char == c
-        last_count += 1
-      else
-        result += append_string(last_count, last_char)
-        last_char = c
-        last_count = 1
-      end
-    end
-
-    return result += append_string(last_count, last_char)
+    s.scan(/((.)\2*)/).map { |m| (m[0].size < 2 ? "" : m[0].size.to_s) + m[0][0] }.join
   end
 
-  def self.decode_2(s : String) : String
+  def self.decode(s : String) : String
     counting, n, result = false, "", ""
-
     s.each_char do |c|
       if c.ascii_number?
         n += c
@@ -29,34 +16,7 @@ class RunLengthEncoding
         n = ""
         counting = false
       end
-
     end
-
     result
-  end
-
-  def self.decode(s : String) : String
-    n, result = "", ""
-
-    i = 0
-    until i == s.size
-      if s[i].ascii_number?
-        until s[i+1].ascii_letter?
-          n += s[i]
-          i += 1
-        end
-        result += s[i].to_s * n.to_i
-      elsif
-        result += s[i]
-
-      i += 1
-    end
-
-    result
-  end
-
-  private def self.append_string(count : Int32, char : Char) : String
-    return "" if count == 0
-    count == 1 ? "#{char}" : "#{count}#{char}"
   end
 end
