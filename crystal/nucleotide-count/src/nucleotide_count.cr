a42d082ad716ac18f57a8cd44f49b00048faef57
Jeddi212 <jedediahf51@gmail.com>
2023-04-08 17:20:54 +0700

    🛳 :: second solution

--- crystal/nucleotide-count/src/nucleotide_count.cr
+++ crystal/nucleotide-count/src/nucleotide_count.cr
@@ -3,4 +3,9 @@ module NucleotideCount
     raise ArgumentError.new if nucleotides.matches?(/[^ACGT]/)
     { 'A' => nucleotides.count('A'), 'C' => nucleotides.count('C'), 'G' => nucleotides.count('G'), 'T' => nucleotides.count('T') }
   end
+
+  def self.nucleotide_counts_two(nucleotides : String) : Hash(Char, Int32)
+    raise ArgumentError.new if nucleotides.matches?(/[^ACGT]/)
+    {'A' => 0, 'C' => 0, 'G' => 0, 'T' => 0}.merge(nucleotides.chars.tally)
+  end
 end
