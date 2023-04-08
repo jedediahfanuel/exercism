ac17d65a8e7e47db26281aa954df8f75db88d50b
Jeddi212 <jedediahf51@gmail.com>
2023-04-08 20:36:03 +0700

    🥠 :: update rna transaltion spec

--- crystal/protein-translation/spec/protein_translation_spec.cr
+++ crystal/protein-translation/spec/protein_translation_spec.cr
@@ -2,95 +2,129 @@ require "spec"
 require "../src/*"
 
 describe "ProteinTranslation" do
-  it "Methionine RNA sequence" do
-    ProteinTranslation.proteins("AUG").should eq(["Methionine"])
+  it "Empty RNA sequence results in no proteins" do
+    ProteinTranslation.proteins("").should eq([] of String)
   end
 
-  it "Phenylalanine RNA sequence 1" do
-    ProteinTranslation.proteins("UUU").should eq(["Phenylalanine"])
+  pending "Methionine RNA sequence" do
+    ProteinTranslation.proteins("AUG").should eq(["Methionine"] of String)
   end
 
-  it "Phenylalanine RNA sequence 2" do
-    ProteinTranslation.proteins("UUC").should eq(["Phenylalanine"])
+  pending "Phenylalanine RNA sequence 1" do
+    ProteinTranslation.proteins("UUU").should eq(["Phenylalanine"] of String)
   end
 
-  it "Leucine RNA sequence 1" do
-    ProteinTranslation.proteins("UUA").should eq(["Leucine"])
+  pending "Phenylalanine RNA sequence 2" do
+    ProteinTranslation.proteins("UUC").should eq(["Phenylalanine"] of String)
   end
 
-  it "Leucine RNA sequence 2" do
-    ProteinTranslation.proteins("UUG").should eq(["Leucine"])
+  pending "Leucine RNA sequence 1" do
+    ProteinTranslation.proteins("UUA").should eq(["Leucine"] of String)
   end
 
-  it "Serine RNA sequence 1" do
-    ProteinTranslation.proteins("UCU").should eq(["Serine"])
+  pending "Leucine RNA sequence 2" do
+    ProteinTranslation.proteins("UUG").should eq(["Leucine"] of String)
   end
 
-  it "Serine RNA sequence 2" do
-    ProteinTranslation.proteins("UCC").should eq(["Serine"])
+  pending "Serine RNA sequence 1" do
+    ProteinTranslation.proteins("UCU").should eq(["Serine"] of String)
   end
 
-  it "Serine RNA sequence 3" do
-    ProteinTranslation.proteins("UCA").should eq(["Serine"])
+  pending "Serine RNA sequence 2" do
+    ProteinTranslation.proteins("UCC").should eq(["Serine"] of String)
   end
 
-  it "Serine RNA sequence 4" do
-    ProteinTranslation.proteins("UCG").should eq(["Serine"])
+  pending "Serine RNA sequence 3" do
+    ProteinTranslation.proteins("UCA").should eq(["Serine"] of String)
   end
 
-  it "Tyrosine RNA sequence 1" do
-    ProteinTranslation.proteins("UAU").should eq(["Tyrosine"])
+  pending "Serine RNA sequence 4" do
+    ProteinTranslation.proteins("UCG").should eq(["Serine"] of String)
   end
 
-  it "Tyrosine RNA sequence 2" do
-    ProteinTranslation.proteins("UAC").should eq(["Tyrosine"])
+  pending "Tyrosine RNA sequence 1" do
+    ProteinTranslation.proteins("UAU").should eq(["Tyrosine"] of String)
   end
 
-  it "Cysteine RNA sequence 1" do
-    ProteinTranslation.proteins("UGU").should eq(["Cysteine"])
+  pending "Tyrosine RNA sequence 2" do
+    ProteinTranslation.proteins("UAC").should eq(["Tyrosine"] of String)
   end
 
-  it "Cysteine RNA sequence 2" do
-    ProteinTranslation.proteins("UGC").should eq(["Cysteine"])
+  pending "Cysteine RNA sequence 1" do
+    ProteinTranslation.proteins("UGU").should eq(["Cysteine"] of String)
   end
 
-  it "Tryptophan RNA sequence" do
-    ProteinTranslation.proteins("UGG").should eq(["Tryptophan"])
+  pending "Cysteine RNA sequence 2" do
+    ProteinTranslation.proteins("UGC").should eq(["Cysteine"] of String)
   end
 
-  it "STOP codon RNA sequence 1" do
+  pending "Tryptophan RNA sequence" do
+    ProteinTranslation.proteins("UGG").should eq(["Tryptophan"] of String)
+  end
+
+  pending "STOP codon RNA sequence 1" do
     ProteinTranslation.proteins("UAA").should eq([] of String)
   end
 
-  it "STOP codon RNA sequence 2" do
+  pending "STOP codon RNA sequence 2" do
     ProteinTranslation.proteins("UAG").should eq([] of String)
   end
 
-  it "STOP codon RNA sequence 3" do
+  pending "STOP codon RNA sequence 3" do
     ProteinTranslation.proteins("UGA").should eq([] of String)
   end
 
-  it "Translate RNA strand into correct protein list" do
-    ProteinTranslation.proteins("AUGUUUUGG").should eq(["Methionine", "Phenylalanine", "Tryptophan"])
+  pending "Sequence of two protein codons translates into proteins" do
+    ProteinTranslation.proteins("UUUUUU").should eq(["Phenylalanine", "Phenylalanine"] of String)
   end
 
-  it "Translation stops if STOP codon at beginning of sequence" do
+  pending "Sequence of two different protein codons translates into proteins" do
+    ProteinTranslation.proteins("UUAUUG").should eq(["Leucine", "Leucine"] of String)
+  end
+
+  pending "Translate RNA strand into correct protein list" do
+    ProteinTranslation.proteins("AUGUUUUGG").should eq(["Methionine", "Phenylalanine", "Tryptophan"] of String)
+  end
+
+  pending "Translation stops if STOP codon at beginning of sequence" do
     ProteinTranslation.proteins("UAGUGG").should eq([] of String)
   end
 
-  it "Translation stops if STOP codon at end of two-codon sequence" do
-    ProteinTranslation.proteins("UGGUAG").should eq(["Tryptophan"])
+  pending "Translation stops if STOP codon at end of two-codon sequence" do
+    ProteinTranslation.proteins("UGGUAG").should eq(["Tryptophan"] of String)
   end
 
-  it "Translation stops if STOP codon at end of three-codon sequence" do
-    ProteinTranslation.proteins("AUGUUUUAA").should eq(["Methionine", "Phenylalanine"])
+  pending "Translation stops if STOP codon at end of three-codon sequence" do
+    ProteinTranslation.proteins("AUGUUUUAA").should eq(["Methionine", "Phenylalanine"] of String)
   end
 
-  it "Translation stops if STOP codon in middle of three-codon sequence" do
-    ProteinTranslation.proteins("UGGUAGUGG").should eq(["Tryptophan"])
+  pending "Translation stops if STOP codon in middle of three-codon sequence" do
+    ProteinTranslation.proteins("UGGUAGUGG").should eq(["Tryptophan"] of String)
   end
 
-  it "Translation stops if STOP codon in middle of six-codon sequence" do
-    ProteinTranslation.proteins("UGGUGUUAUUAAUGGUUU").should eq(["Tryptophan", "Cysteine", "Tyrosine"])
+  pending "Translation stops if STOP codon in middle of six-codon sequence" do
+    ProteinTranslation.proteins("UGGUGUUAUUAAUGGUUU").should eq(["Tryptophan", "Cysteine", "Tyrosine"] of String)
+  end
+
+  pending "Non-existing codon can't translate" do
+    expect_raises(ArgumentError) do
+      ProteinTranslation.proteins("AAA")
+    end
+  end
+
+  pending "Unknown amino acids, not part of a codon, can't translate" do
+    expect_raises(ArgumentError) do
+      ProteinTranslation.proteins("XYZ")
+    end
+  end
+
+  pending "Incomplete RNA sequence can't translate" do
+    expect_raises(ArgumentError) do
+      ProteinTranslation.proteins("AUGU")
+    end
+  end
+
+  pending "Incomplete RNA sequence can translate if valid until a STOP codon" do
+    ProteinTranslation.proteins("UUCUUCUAAUGGU").should eq(["Phenylalanine", "Phenylalanine"] of String)
   end
 end
