efb093625ad066da0196b95d0172726785e2c6c6
Jeddi212 <jedediahf51@gmail.com>
2023-04-08 19:20:10 +0700

    🥡 :: join first

--- crystal/phone-number/src/phone_number.cr
+++ crystal/phone-number/src/phone_number.cr
@@ -1,8 +1,8 @@
 module PhoneNumber
   def self.clean(phrase : String) : String
-    num = phrase.chars.select(&.ascii_number?).map(&.to_i)
-    return num[1..].join if num.size == 11 && num[0] == 1 && num[1] > 1 && num[4] > 1
-    return num.join      if num.size == 10 &&                num[0] > 1 && num[3] > 1
-    raise ArgumentError.new
+    num  = phrase.chars.select(&.ascii_number?).join
+    num  = num.delete_at 0 if num.starts_with? '1'
+    return num             if num.size  == 10 && num[0].to_i > 1 && num[3].to_i > 1
+    raise  ArgumentError.new
   end
 end
