51143ff4ee1c961cfe03e7379be0566e8ecf53e4
Jeddi212 <jedediahf51@gmail.com>
2023-04-08 23:20:47 +0700

    🚜 :: sublist complete

--- crystal/sublist/src/sublist.cr
+++ crystal/sublist/src/sublist.cr
@@ -1,12 +1,12 @@
 module Sublist
-  enum Classification
-    Equal
-    Unequal
-    Sublist
-    Superlist
-  end
+  enum Classification ; Equal ; Unequal ; Sublist ; Superlist ; end
 
   def self.classify(list1 : Array(Int32), list2 : Array(Int32)) : Classification
-    # Write your code for the 'Sublist' exercise in this file.
+    return Classification::Equal     if list1 == list2
+    return Classification::Sublist   if list1.empty?
+    return Classification::Sublist   if list1.size < list2.size && list2.each_cons(list1.size).any? list1
+    return Classification::Superlist if list2.empty?
+    return Classification::Superlist if list1.size > list2.size && list1.each_cons(list2.size).any? list2
+    return Classification::Unequal 
   end
 end
