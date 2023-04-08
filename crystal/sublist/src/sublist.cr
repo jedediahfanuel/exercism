8daf26c3bc491ff555585751015ffb0c8a99a7c0
Jeddi212 <jedediahf51@gmail.com>
2023-04-08 23:31:47 +0700

    🦈 :: use regex join string

--- crystal/sublist/src/sublist.cr
+++ crystal/sublist/src/sublist.cr
@@ -1,12 +1,10 @@
 module Sublist
   enum Classification ; Equal ; Unequal ; Sublist ; Superlist ; end
 
-  def self.classify(list1 : Array(Int32), list2 : Array(Int32)) : Classification
-    return Classification::Equal     if list1 == list2
-    return Classification::Sublist   if list1.empty?
-    return Classification::Sublist   if list1.size < list2.size && list2.each_cons(list1.size).any? list1
-    return Classification::Superlist if list2.empty?
-    return Classification::Superlist if list1.size > list2.size && list1.each_cons(list2.size).any? list2
+  def self.classify(x : Array(Int32), y : Array(Int32)) : Classification
+    return Classification::Equal     if x == y
+    return Classification::Sublist   if x.size < y.size && y.join(" ").matches? Regex.new x.join " "
+    return Classification::Superlist if x.size > y.size && x.join(" ").matches? Regex.new y.join " "
     return Classification::Unequal 
   end
 end
