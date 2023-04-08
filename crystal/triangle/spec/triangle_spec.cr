a34b57dab94af69fc91bdb5ddfd98aa7fa331be8
Jeddi212 <jedediahf51@gmail.com>
2023-04-08 20:28:42 +0700

    😌 :: update triangle exercise

--- crystal/triangle/spec/triangle_spec.cr
+++ crystal/triangle/spec/triangle_spec.cr
@@ -3,15 +3,15 @@ require "../src/*"
 
 describe "Triangle" do
   it "all sides are equal" do
-    Triangle.new({2, 2, 2}).equilateral?.should eq(true)
+    Triangle.new({2, 2, 2}).equilateral?.should be_true
   end
 
   it "any side is unequal" do
-    Triangle.new({2, 3, 2}).equilateral?.should eq(false)
+    Triangle.new({2, 3, 2}).equilateral?.should be_false
   end
 
   it "no sides are equal" do
-    Triangle.new({5, 4, 6}).equilateral?.should eq(false)
+    Triangle.new({5, 4, 6}).equilateral?.should be_false
   end
 
   it "all zero sides is not a triangle" do
@@ -21,27 +21,27 @@ describe "Triangle" do
   end
 
   it "sides may be floats" do
-    Triangle.new({0.5, 0.5, 0.5}).equilateral?.should eq(true)
+    Triangle.new({0.5, 0.5, 0.5}).equilateral?.should be_true
   end
 
   it "last two sides are equal" do
-    Triangle.new({3, 4, 4}).isosceles?.should eq(true)
+    Triangle.new({3, 4, 4}).isosceles?.should be_true
   end
 
   it "first two sides are equal" do
-    Triangle.new({4, 4, 3}).isosceles?.should eq(true)
+    Triangle.new({4, 4, 3}).isosceles?.should be_true
   end
 
   it "first and last sides are equal" do
-    Triangle.new({4, 3, 4}).isosceles?.should eq(true)
+    Triangle.new({4, 3, 4}).isosceles?.should be_true
   end
 
   it "equilateral triangles are also isosceles" do
-    Triangle.new({4, 4, 4}).isosceles?.should eq(true)
+    Triangle.new({4, 4, 4}).isosceles?.should be_true
   end
 
   it "no sides are equal" do
-    Triangle.new({2, 3, 4}).isosceles?.should eq(false)
+    Triangle.new({2, 3, 4}).isosceles?.should be_false
   end
 
   it "first triangle inequality violation" do
@@ -63,19 +63,27 @@ describe "Triangle" do
   end
 
   it "sides may be floats" do
-    Triangle.new({0.5, 0.4, 0.5}).isosceles?.should eq(true)
+    Triangle.new({0.5, 0.4, 0.5}).isosceles?.should be_true
   end
 
   it "no sides are equal" do
-    Triangle.new({5, 4, 6}).scalene?.should eq(true)
+    Triangle.new({5, 4, 6}).scalene?.should be_true
   end
 
   it "all sides are equal" do
-    Triangle.new({4, 4, 4}).scalene?.should eq(false)
+    Triangle.new({4, 4, 4}).scalene?.should be_false
   end
 
-  it "two sides are equal" do
-    Triangle.new({4, 4, 3}).scalene?.should eq(false)
+  it "first and second sides are equal" do
+    Triangle.new({4, 4, 3}).scalene?.should be_false
+  end
+
+  it "first and third sides are equal" do
+    Triangle.new({3, 4, 3}).scalene?.should be_false
+  end
+
+  it "second and third sides are equal" do
+    Triangle.new({4, 3, 3}).scalene?.should be_false
   end
 
   it "may not violate triangle inequality" do
@@ -85,6 +93,6 @@ describe "Triangle" do
   end
 
   it "sides may be floats" do
-    Triangle.new({0.5, 0.4, 0.6}).scalene?.should eq(true)
+    Triangle.new({0.5, 0.4, 0.6}).scalene?.should be_true
   end
 end
--- crystal/triangle/src/triangle.cr
+++ crystal/triangle/src/triangle.cr
@@ -1,9 +1,7 @@
 class Triangle
   alias Flint = Int32 | Float64
   def initialize(@sides : Tuple(Flint, Flint, Flint))
-    raise ArgumentError.new if isInvalid(@sides[0], @sides[1], @sides[2])
-    self
-  end
+    raise ArgumentError.new if isInvalid(@sides[0], @sides[1], @sides[2]) end
   
   def isInvalid(a, b, c) : Bool
     case {((a + b) > c), ((b + c) > a), ((c + a) > b)}
@@ -12,28 +10,7 @@ class Triangle
     end
   end
   
-  def equilateral? : Bool
-    @sides[0] == @sides[1] && @sides[1] == @sides[2]
-  end
-  
-  def isosceles?
-    @sides.tally.size < 3
-  end
-  
-  def scalene?
-    @sides[0] != @sides[1] && @sides[1] != @sides[2]
-  end
-  
-  def isosceles_two?
-    @sides.to_set.size >= 1 && !scalene?
-  end
-  
-  def scalene_two?
-    @sides.to_set.size == 3
-  end
-  
-  def inequality_two?
-    x = @sides.to_a.sort
-    x[0] + x[1] < x[2]
-  end  
+  def equilateral? ; @sides[0] == @sides[1] && @sides[1] == @sides[2]                            end 
+  def isosceles?   ; @sides.tally.size < 3                                                       end
+  def scalene?     ; @sides[0] != @sides[1] && @sides[1] != @sides[2] && @sides[0] != @sides[2]  end
 end
