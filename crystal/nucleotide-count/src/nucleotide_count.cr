fd559b4ecfae8ac1c29b4e79cd85a62e4dacda70
Jeddi212 <jedediahf51@gmail.com>
2023-04-08 17:15:30 +0700

    📁 :: nucleotide done

--- crystal/nucleotide-count/src/nucleotide_count.cr
+++ crystal/nucleotide-count/src/nucleotide_count.cr
@@ -1,5 +1,6 @@
 module NucleotideCount
   def self.nucleotide_counts(nucleotides : String) : Hash(Char, Int32)
-    # Write your code for the 'Nucleotide Count' exercise in this file.
+    raise ArgumentError.new if nucleotides.matches?(/[^ACGT]/)
+    { 'A' => nucleotides.count('A'), 'C' => nucleotides.count('C'), 'G' => nucleotides.count('G'), 'T' => nucleotides.count('T') }
   end
 end
