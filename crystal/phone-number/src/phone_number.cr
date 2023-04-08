ce293bfefa9c937c5890dbb139ba2a44a4ca95ef
Jeddi212 <jedediahf51@gmail.com>
2023-04-08 18:30:03 +0700

    🥍 :: phone number done

--- crystal/phone-number/src/phone_number.cr
+++ crystal/phone-number/src/phone_number.cr
@@ -1,5 +1,8 @@
 module PhoneNumber
   def self.clean(phrase : String) : String
-    # Write your code for the 'Phone Number' exercise in this file.
+    num = phrase.chars.select(&.ascii_number?).map(&.to_i)
+    return num[1..].join if num.size == 11 && num[0] == 1 && num[1] > 1 && num[4] > 1
+    return num.join      if num.size == 10 &&                num[0] > 1 && num[3] > 1
+    raise ArgumentError.new
   end
 end
