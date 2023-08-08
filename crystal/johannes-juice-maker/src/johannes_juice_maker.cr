78e3051bcb69dd01145b43f3d50175e2684a8991
Jeddi212 <jedediahf51@gmail.com>
2023-08-09 00:14:53 +0700

    🧃 :: juice

--- crystal/johannes-juice-maker/src/johannes_juice_maker.cr
+++ crystal/johannes-juice-maker/src/johannes_juice_maker.cr
@@ -1,1 +1,11 @@
-# To Do: Define the class JuiceMaker
+class JuiceMaker
+  def self.debug_light_on? ; true end
+  def initialize(@fluid : Int32, @running = false) end
+  def start ; @running = true end
+  def running? ; @running end
+  def add_fluid(q : Int32) @fluid += q end
+  def stop(time : Int32)
+    @fluid -= (time * 5) if @fluid > 4
+    @running = false
+  end
+end
