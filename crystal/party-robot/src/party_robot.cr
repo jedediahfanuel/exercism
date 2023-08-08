123246e9dfca1520d74c9493449f69aa4fa86200
Jeddi212 <jedediahf51@gmail.com>
2023-08-09 00:36:38 +0700

    🚎 :: party robot done

--- crystal/party-robot/src/party_robot.cr
+++ crystal/party-robot/src/party_robot.cr
@@ -1,13 +1,15 @@
 class PartyRobot
   def self.welcome(name : String) : String
-    raise "Please implement the PartyRobot.welcome method"
+    "Welcome to my party, #{name}!"
   end
 
   def self.happy_birthday(name : String, age : Int) : String
-    raise "Please implement the PartyRobot.happy_birthday method"
+    "Happy birthday #{name}! You are now #{age} years old!"
   end
 
   def self.assign_table(name : String, direction : String, table : Int, distance : Float64, neighbor : String) : String
-    raise "Please implement the PartyRobot.assign_table method"
+"#{self.welcome(name)}
+You have been assigned to table #{table}. Your table is #{direction}, exactly #{distance.round(1)} meters from here.
+You will be sitting next to #{neighbor.[0]}#{neighbor.[-1]}."
   end
 end
