03770bdb813951419a1f795bbfa90f2898080aae
Jeddi212 <jedediahf51@gmail.com>
2023-08-10 01:08:03 +0700

    🐢 :: simplify code

--- crystal/interest-is-interesting/src/interest_is_interesting.cr
+++ crystal/interest-is-interesting/src/interest_is_interesting.cr
@@ -1,10 +1,9 @@
 class SavingsAccount
   def self.interest_rate(balance)
-    case balance
-    when .< 0    then 3.213
-    when .< 1000 then 0.5
-    when .< 5000 then 1.621
-    else              2.475 end
+    return 3.213 if balance < 0
+    return 0.5   if balance < 1000
+    return 1.621 if balance < 5000
+    return 2.475
   end
 
   def self.interest(balance)
@@ -12,16 +11,13 @@ class SavingsAccount
   end
 
   def self.annual_balance_update(balance)
-    return 0 if balance.zero?
-    balance + interest(balance)
+    balance.zero? ? 0 : balance + interest(balance)
   end
 
   def self.years_before_desired_balance(current_balance, target_balance)
-    years = 0
-    while true
+    (0..).each_with_index do |years|
       break years if current_balance >= target_balance
       current_balance = annual_balance_update(current_balance)
-      years += 1
     end
   end
 end
