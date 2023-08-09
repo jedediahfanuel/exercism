07ccc56843f7390f65cd15321092596f904b1b7b
Jeddi212 <jedediahf51@gmail.com>
2023-08-10 00:54:05 +0700

    🚃 :: interest done

--- crystal/interest-is-interesting/src/interest_is_interesting.cr
+++ crystal/interest-is-interesting/src/interest_is_interesting.cr
@@ -1,17 +1,27 @@
 class SavingsAccount
   def self.interest_rate(balance)
-    raise "Please implement the SavingsAccount#interest_rate method"
+    case balance
+    when .< 0    then 3.213
+    when .< 1000 then 0.5
+    when .< 5000 then 1.621
+    else              2.475 end
   end
 
   def self.interest(balance)
-    raise "Please implement the SavingsAccount#interest method"
+    interest_rate(balance) * balance / 100
   end
 
   def self.annual_balance_update(balance)
-    raise "Please implement the SavingsAccount#annual_balance_update method"
+    return 0 if balance.zero?
+    balance + interest(balance)
   end
 
   def self.years_before_desired_balance(current_balance, target_balance)
-    raise "Please implement the SavingsAccount#years_before_desired_balance method"
+    years = 0
+    while true
+      break years if current_balance >= target_balance
+      current_balance = annual_balance_update(current_balance)
+      years += 1
+    end
   end
 end
