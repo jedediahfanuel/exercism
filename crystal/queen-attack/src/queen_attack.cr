bbe252d191260cf4c66f32528a653f56928c4ed7
Jeddi212 <jedediahf51@gmail.com>
2023-08-15 20:39:15 +0700

    🌇 :: update

--- crystal/queen-attack/src/queen_attack.cr
+++ crystal/queen-attack/src/queen_attack.cr
@@ -1,5 +1,5 @@
 class Queen
-  property x, y
-  def initialize(@x : Int32, @y : Int32) raise ArgumentError.new if x < 0 || x > 7 || y < 0 || y > 7 end
-  def can_attack?(q : Queen) x == q.x || y == q.y || (x - q.x).abs == (y - q.y).abs ? true : false end
+  property x : Int32, y : Int32
+  def initialize(@x, @y) raise ArgumentError.new if x < 0 || x > 7 || y < 0 || y > 7 end
+  def can_attack?(q : Queen) x == q.x || y == q.y || (x - q.x).abs == (y - q.y).abs end
 end
