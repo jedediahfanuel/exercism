97288974e1442fd8c18470a3201b9153c296810f
Jeddi212 <jedediahf51@gmail.com>
2023-08-15 17:27:33 +0700

    🎍 :: saddle points done

--- crystal/saddle-points/src/saddle_points.cr
+++ crystal/saddle-points/src/saddle_points.cr
@@ -1,5 +1,19 @@
 module SaddlePoints
   def self.saddle_points(matrix : Array(Array(Int32))) : Set(NamedTuple(row: Int32, column: Int32))
-    # Write your code for the 'SaddlePoints' exercise in this file.
+    result = Set.new [] of NamedTuple(row: Int32, column: Int32)
+    return result if matrix.empty?
+    
+    row_max, col_min = matrix.map &.max, matrix.transpose.map &.min
+
+    row_max.each_with_index do |row, i|
+      next if row != row_max.min
+
+      col_min.each_with_index do |col, j|
+        next if col != row
+        result << { row: i+1, column: j+1 }
+      end
+    end
+    
+    result
   end
 end
