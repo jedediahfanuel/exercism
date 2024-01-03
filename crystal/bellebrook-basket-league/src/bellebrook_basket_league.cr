d98a0e2b86bd36cccb1531f90d13bd10b0f0ac27
jedediahfanuel <jedediahf51@gmail.com>
2024-01-03 20:28:11 +0700

    🦟 :: bellebrook basket league

---
+++ crystal/bellebrook-basket-league/src/bellebrook_basket_league.cr
@@ -0,0 +1,18 @@
+module TicketingReservation
+  def tickets_available ; @tickets_available end
+
+  def order_ticket?
+    return false if tickets_available < 100
+    @tickets_available -= 1 ; true
+  end
+
+  def order_message(name : String)
+    return "#{name}, your purchase was successful, your ticket number is ##{tickets_available + 1}, and the game is played at the #{@stadium} stadium." if order_ticket?
+    "#{name}, your purchase was unsuccessful, there are not enough tickets available."
+  end
+end
+
+class TicketSystem
+  def initialize(@tickets_available : Int32, @stadium : String) end
+  include TicketingReservation
+end
