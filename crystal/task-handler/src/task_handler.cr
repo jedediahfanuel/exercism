f5280c1490771efbda0c358cee0157ab0d1a36f4
jedediahfanuel <jedediahf51@gmail.com>
2024-07-07 15:55:35 +0700

     :: task handler - procs

---
+++ crystal/task-handler/src/task_handler.cr
@@ -0,0 +1,15 @@
+class TaskHandler
+  @task_condition_logic : Proc(Int32, Bool)
+  def initialize(@tasks : Array(String))
+    @task_condition_logic = -> (len : Int32) { len >= 0 }
+  end
+
+  def update_task_condition_logic(&block : Int32 -> Bool)
+    @task_condition_logic = block
+  end
+
+  def execute(priority : Int32)
+    return "Could not complete " + @tasks.first unless @task_condition_logic.call(priority)
+    "Completed " + @tasks.shift(priority).first
+  end
+end
