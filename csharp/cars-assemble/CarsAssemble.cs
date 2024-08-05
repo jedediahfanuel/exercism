44ddcccc809aacf7482d969e833a1a2b2cf93783
jedediahfanuel <jedediahf51@gmail.com>
2024-08-05 23:59:38 +0700

    🫔 :: another pattern matching logic

--- csharp/cars-assemble/CarsAssemble.cs
+++ csharp/cars-assemble/CarsAssemble.cs
@@ -4,11 +4,11 @@ static class AssemblyLine
 {
     public static double SuccessRate(int speed) => speed switch
     {
-          0 => 0   ,
-        < 5 => 1   ,
-        < 9 =>  .9 ,
-          9 =>  .8 ,
-          _ =>  .77,
+        <= 0 => 0   ,
+        <= 4 => 1   ,
+        <= 8 =>  .9 ,
+        <= 9 =>  .8 ,
+           _ =>  .77,
     };
     
     public static double ProductionRatePerHour(int speed) =>
