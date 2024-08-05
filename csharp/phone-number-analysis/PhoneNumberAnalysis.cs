2106ea0596cb07e777a36ebc00f9af0f9bc67f93
jedediahfanuel <jedediahf51@gmail.com>
2024-08-05 23:25:51 +0700

    🧀  :: another example

--- csharp/phone-number-analysis/PhoneNumberAnalysis.cs
+++ csharp/phone-number-analysis/PhoneNumberAnalysis.cs
@@ -8,6 +8,10 @@ public static class PhoneNumber
         return (nums[0] == "212", nums[1] == "555", nums[2]);
     }
 
+    // public static (bool IsNewYork, bool IsFake, string LocalNumber) Analyze(string phoneNumber) =>
+    //     // we don't care about validating input for this one
+    //     phoneNumber.Split('-') switch { var tup => (tup[0] == "212", tup[1] == "555", tup[2]) };
+
     public static bool IsFake((bool IsNewYork, bool IsFake, string LocalNumber) phoneNumberInfo) =>
         phoneNumberInfo.IsFake;
 }
