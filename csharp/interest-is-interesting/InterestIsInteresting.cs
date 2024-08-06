6fc66d2744d22596c7fd422ce0d3db19889125ab
jedediahfanuel <jedediahf51@gmail.com>
2024-08-06 20:48:41 +0700

    🔈️ :: interest is interesting done

--- csharp/interest-is-interesting/InterestIsInteresting.cs
+++ csharp/interest-is-interesting/InterestIsInteresting.cs
@@ -2,23 +2,29 @@ using System;
 
 static class SavingsAccount
 {
-    public static float InterestRate(decimal balance)
+    public static float InterestRate(decimal balance) => balance switch 
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.InterestRate() method");
-    }
+        <    0 => 3.213f,
+        < 1000 => 0.5f  ,
+        < 5000 => 1.621f,
+        _      => 2.475f,
+    };
 
-    public static decimal Interest(decimal balance)
-    {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.Interest() method");
-    }
 
-    public static decimal AnnualBalanceUpdate(decimal balance)
-    {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.AnnualBalanceUpdate() method");
-    }
+    public static decimal Interest(decimal balance) => 
+        (decimal)InterestRate(balance) * balance / 100;
+
+    public static decimal AnnualBalanceUpdate(decimal balance) =>
+        Interest(balance) + balance;
 
     public static int YearsBeforeDesiredBalance(decimal balance, decimal targetBalance)
     {
-        throw new NotImplementedException("Please implement the (static) SavingsAccount.YearsBeforeDesiredBalance() method");
+        int count = 0;
+        while (balance < targetBalance)
+        {
+            balance = AnnualBalanceUpdate(balance);
+            count++;
+        }
+        return count;
     }
 }
