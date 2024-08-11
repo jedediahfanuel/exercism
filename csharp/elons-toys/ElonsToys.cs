1cc33a8f428a5421d01008c4e6ba832480ed8175
jedediahfanuel <jedediahf51@gmail.com>
2024-08-11 15:19:18 +0700

    💶 :: change to private variable

--- csharp/elons-toys/ElonsToys.cs
+++ csharp/elons-toys/ElonsToys.cs
@@ -2,11 +2,11 @@ using System;
 
 class RemoteControlCar
 {
-    public int Distance = 0;
-    public int Battery = 100;
+    private int _distance = 0;
+    private int _battery = 100;
     
     public static RemoteControlCar Buy() => new RemoteControlCar();
-    public string DistanceDisplay() => $"Driven {Distance} meters";
-    public string BatteryDisplay() => Battery > 0 ? $"Battery at {Battery}%" : $"Battery empty";
-    public void Drive() { if (Battery > 0) Distance += 20; Battery -= 1; }
+    public string DistanceDisplay() => $"Driven {_distance} meters";
+    public string BatteryDisplay() => _battery > 0 ? $"Battery at {_battery}%" : $"Battery empty";
+    public void Drive() { if (_battery > 0) _distance += 20; _battery -= 1; }
 }
