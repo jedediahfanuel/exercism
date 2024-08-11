bda7590d538d509ce124813f6f9288818fee9324
jedediahfanuel <jedediahf51@gmail.com>
2024-08-11 17:07:59 +0700

    📉 :: booking for beauty

--- csharp/need-for-speed/NeedForSpeed.cs
+++ csharp/need-for-speed/NeedForSpeed.cs
@@ -1,28 +1,18 @@
 class RemoteControlCar {
-    private int battery = 100;
-    private int distans = 0;
-    public  int speed;
-    public  int drain;
+    private int battery = 100; private int distans = 0; public  int speed; public  int drain;
 
     public RemoteControlCar(int speed, int drain) { this.speed = speed; this.drain = drain; }
-
     public bool BatteryDrained() => this.battery < this.drain;
-
     public int  DistanceDriven() => this.distans;
 
     public void Drive() {
         if (!this.BatteryDrained()) this.distans += this.speed;
-        if (!this.BatteryDrained()) this.battery -= this.drain;
-    }
+        if (!this.BatteryDrained()) this.battery -= this.drain; }
 
-    public static RemoteControlCar Nitro() => new RemoteControlCar(50, 4);
-}
+    public static RemoteControlCar Nitro() => new (50, 4); }
 
-class RaceTrack
-{
+class RaceTrack {
     private int length;
 
     public RaceTrack(int length) => this.length = length;
-
-    public bool TryFinishTrack(RemoteControlCar car) => (float) this.length / car.speed <= (float) 100 / car.drain;
-}
+    public bool TryFinishTrack(RemoteControlCar car) => (float) this.length / car.speed <= (float) 100 / car.drain; }
