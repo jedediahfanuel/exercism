6e57ee77c241262db51f5869d69f59908d813638
jedediahfanuel <jedediahf51@gmail.com>
2024-08-11 18:15:03 +0700

    📏 :: schedule function

--- csharp/booking-up-for-beauty/BookingUpForBeauty.cs
+++ csharp/booking-up-for-beauty/BookingUpForBeauty.cs
@@ -2,9 +2,57 @@ using System;
 
 static class Appointment
 {
+    private static string[] Months = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+    
     public static DateTime Schedule(string appointmentDateDescription)
     {
-        throw new NotImplementedException("Please implement the (static) Appointment.Schedule() method");
+        string[] values = appointmentDateDescription.Split(' ');
+        string[] date; string[] time;
+
+        int yy, mo, dd, hh, mm, ss;
+
+        switch (values.Length)
+        {
+            case 2:
+                date = values[0].Split('/');
+                time = values[1].Split(':');
+
+                yy = int.Parse(date[2]);
+                mo = int.Parse(date[0]);
+                dd = int.Parse(date[1]);
+
+                hh = int.Parse(time[0]);
+                mm = int.Parse(time[1]);
+                ss = int.Parse(time[2]);
+
+                break;
+            case 4:
+                time = values[3].Split(':');
+
+                yy = int.Parse(values[2]);
+                mo = Array.IndexOf(Months, values[0]) + 1;
+                dd = int.Parse(values[1][..(values[1].Length - 1)]);
+
+                hh = int.Parse(time[0]);
+                mm = int.Parse(time[1]);
+                ss = int.Parse(time[2]);
+
+                break;
+            default:
+                time = values[4].Split(':');
+
+                yy = int.Parse(values[3]);
+                mo = Array.IndexOf(Months, values[1]) + 1;
+                dd = int.Parse(values[2][..(values[2].Length - 1)]);
+
+                hh = int.Parse(time[0]);
+                mm = int.Parse(time[1]);
+                ss = int.Parse(time[2]);
+
+                break;
+        }
+
+        return new DateTime(yy,mo,dd,hh,mm,ss);
     }
 
     public static bool HasPassed(DateTime appointmentDate)
