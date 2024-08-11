d680d6fccf50f7de65921bcc4d8cdbef4f03beaa
jedediahfanuel <jedediahf51@gmail.com>
2024-08-11 15:28:02 +0700

    🗝️  :: truncate more lines

--- csharp/elons-toys/ElonsToys.cs
+++ csharp/elons-toys/ElonsToys.cs
@@ -1,9 +1,5 @@
-class RemoteControlCar {
-    private int _distance = 0;
-    private int _battery = 100;
-    
+class RemoteControlCar { private int _distance = 0; private int _battery = 100;
     public static RemoteControlCar Buy() => new RemoteControlCar();
     public string DistanceDisplay() => $"Driven {_distance} meters";
     public string BatteryDisplay() => _battery > 0 ? $"Battery at {_battery}%" : $"Battery empty";
-    public void Drive() { if (_battery > 0) _distance += 20; _battery -= 1; }
-}
+    public void Drive() { if (_battery > 0) _distance += 20; _battery -= 1; } }
