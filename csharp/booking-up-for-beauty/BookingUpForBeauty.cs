b53a39b79ad5b5f36765ed34e93f6848e217d7ed
jedediahfanuel <jedediahf51@gmail.com>
2024-08-11 18:31:29 +0700

    📡 :: describe function

--- csharp/booking-up-for-beauty/BookingUpForBeauty.cs
+++ csharp/booking-up-for-beauty/BookingUpForBeauty.cs
@@ -59,10 +59,7 @@ static class Appointment
 
     public static bool IsAfternoonAppointment(DateTime appointmentDate) => appointmentDate.Hour > 11 && appointmentDate.Hour < 18;
 
-    public static string Description(DateTime appointmentDate)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.Description() method");
-    }
+    public static string Description(DateTime appointmentDate) => $"You have an appointment on {appointmentDate}.";
 
     public static DateTime AnniversaryDate()
     {
