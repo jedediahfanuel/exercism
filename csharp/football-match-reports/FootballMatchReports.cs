5ad19d6fba5888254d99dedfffc347d1e1516f8d
jedediahfanuel <jedediahf51@gmail.com>
2024-08-14 22:44:52 +0700

    ⚽️ :: football match reports done

--- csharp/football-match-reports/FootballMatchReports.cs
+++ csharp/football-match-reports/FootballMatchReports.cs
@@ -1,14 +1,24 @@
 using System;
 
-public static class PlayAnalyzer
-{
-    public static string AnalyzeOnField(int shirtNum)
-    {
-        throw new NotImplementedException($"Please implement the (static) PlayAnalyzer.AnalyzeOnField() method");
-    }
+public static class PlayAnalyzer                                                                                       {
 
-    public static string AnalyzeOffField(object report)
-    {
-        throw new NotImplementedException($"Please implement the (static) PlayAnalyzer.AnalyzeOffField() method");
-    }
-}
+    public static string AnalyzeOnField(int shirtNum) => shirtNum switch                                               {
+        1               => "goalie",
+        2               => "left back",
+        3 or 4          => "center back",
+        5               => "right back",
+        (> 5) and (< 9) => "midfielder",
+        9               => "left wing",
+        10              => "striker",
+        11              => "right wing",
+        _ => throw new ArgumentOutOfRangeException(),                                                                  };
+
+    public static string AnalyzeOffField(object report)                                                                {
+        switch (report)                                                                                                {
+            case int num      : return $"There are {num} supporters at the match."                   ;
+            case string str   : return str                                                           ;
+            case Injury inj   : return $"Oh no! {inj.GetDescription()} Medics are on the field."     ;
+            case Incident inc : return inc.GetDescription()                                          ;
+            case Manager man  : return man.Club is null ? $"{man.Name}" : $"{man.Name} ({man.Club})" ;
+            default           : throw new ArgumentException()                                        ;
+                                                                                                                       } } }
