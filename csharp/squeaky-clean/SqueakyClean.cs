6e8abb78e206ed481feeff309f35ed5e41117d73
jedediahfanuel <jedediahf51@gmail.com>
2024-08-14 21:12:16 +0700

    🤔 :: squeaky clean done

--- csharp/squeaky-clean/SqueakyClean.cs
+++ csharp/squeaky-clean/SqueakyClean.cs
@@ -1,9 +1,26 @@
-using System;
+using System; using System.Text;
 
-public static class Identifier
-{
-    public static string Clean(string identifier)
-    {
-        throw new NotImplementedException($"Please implement the (static) Identifier.Clean() method");
-    }
-}
+public static class Identifier                                                                                          {
+
+    public static string Clean(string identifier)                                                                       {
+
+        StringBuilder sb = new StringBuilder();
+        bool camel = false;
+
+        foreach(char c in identifier)                                                                                   {
+
+            if (c.Equals(' '))                                                                                          {
+                sb.Append('_');                                                                                         }
+
+            else if (Char.IsControl(c))                                                                                 {
+                sb.Append("CTRL");                                                                                      }
+
+            else if (camel)                                                                                             {
+                sb.Append(Char.ToUpper(c));                                                                             }
+
+            else if (Char.IsLetter(c) && !Char.IsBetween(c, 'α', 'ω'))                                                  {
+                sb.Append(c);                                                                                           }
+
+            camel = c.Equals('-');                                                                                      }
+
+        return sb.ToString();                                                                                           } }
