b5e732ae9e24b7930165c0423e508f0c7c6ccc34
jedediahfanuel <jedediahf51@gmail.com>
2024-08-19 20:42:03 +0700

    🤏 :: lasagna done

--- java/lasagna/src/main/java/Lasagna.java
+++ java/lasagna/src/main/java/Lasagna.java
@@ -1,9 +1,15 @@
 public class Lasagna {
-    // TODO: define the 'expectedMinutesInOven()' method
+    public int expectedMinutesInOven() { return 40; }
 
-    // TODO: define the 'remainingMinutesInOven()' method
+    public int remainingMinutesInOven(int x) {
+        return expectedMinutesInOven() - x;
+    }
 
-    // TODO: define the 'preparationTimeInMinutes()' method
+    public int preparationTimeInMinutes(int layer) {
+        return layer * 2;
+    }
 
-    // TODO: define the 'totalTimeInMinutes()' method
+    public int totalTimeInMinutes(int layer, int minutes) {
+        return minutes + preparationTimeInMinutes(layer);
+    }
 }
