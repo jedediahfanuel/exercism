07fa5de59f6250183ac8b4774120712654c720d0
jedediahfanuel <jedediahf51@gmail.com>
2024-08-19 22:55:58 +0700

    ✈️  :: log levels done

--- java/log-levels/src/main/java/LogLevels.java
+++ java/log-levels/src/main/java/LogLevels.java
@@ -1,14 +1,14 @@
 public class LogLevels {
     
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLevels.message() method");
+        return logLine.split(":")[1].strip();
     }
 
     public static String logLevel(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLevels.logLevel() method");
+        return logLine.split("\\]")[0].split("\\[")[1].toLowerCase();
     }
 
     public static String reformat(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLevels.reformat() method");
+        return message(logLine) + " (" + logLevel(logLine) + ")";
     }
 }
