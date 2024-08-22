cfdd8892b2cd13008a63dcd28b86f3f281b30445
jedediahfanuel <jedediahf51@gmail.com>
2024-08-22 21:02:41 +0700

    🎭️ :: beauty salon goes global

--- csharp/beauty-salon-goes-global/BeautySalonGoesGlobal.cs
+++ csharp/beauty-salon-goes-global/BeautySalonGoesGlobal.cs
@@ -1,44 +1,33 @@
-using System;
+using System; using System.Globalization;
 
+public enum Location { NewYork, London, Paris }
 
-public enum Location
-{
-    NewYork,
-    London,
-    Paris
-}
-
-public enum AlertLevel
-{
-    Early,
-    Standard,
-    Late
-}
+public enum AlertLevel { Early, Standard, Late }
 
 public static class Appointment
 {
-    public static DateTime ShowLocalTime(DateTime dtUtc)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.ShowLocalTime() method");
-    }
+    public static DateTime ShowLocalTime(DateTime dtUtc) => dtUtc.ToLocalTime();
 
-    public static DateTime Schedule(string appointmentDateDescription, Location location)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.Schedule() method");
-    }
+    public static DateTime Schedule(string appDateDescript, Location location) => location switch {
+    	Location.NewYork => TimeZoneInfo.ConvertTimeToUtc(DateTime.Parse(appDateDescript), TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time")), 
+        Location.London => TimeZoneInfo.ConvertTimeToUtc(DateTime.Parse(appDateDescript), TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time")), 
+		Location.Paris => TimeZoneInfo.ConvertTimeToUtc(DateTime.Parse(appDateDescript), TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time")) };		
 
-    public static DateTime GetAlertTime(DateTime appointment, AlertLevel alertLevel)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.GetAlertTime() method");
-    }
+    public static DateTime GetAlertTime(DateTime appointment, AlertLevel alertLevel) => alertLevel switch {
+		AlertLevel.Early => appointment.Subtract(TimeSpan.Parse("1.00:00:00")),
+		AlertLevel.Standard => appointment.Subtract(TimeSpan.Parse("01:45:00")),
+		AlertLevel.Late => appointment.Subtract(TimeSpan.Parse("00:30:00")) };
 
-    public static bool HasDaylightSavingChanged(DateTime dt, Location location)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.HasDaylightSavingChanged() method");
-    }
+    public static bool HasDaylightSavingChanged(DateTime dt, Location location) => location switch {
+    	Location.NewYork => TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time").IsDaylightSavingTime(dt) ^ TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time").IsDaylightSavingTime(dt.Subtract(TimeSpan.Parse("7.00:00:00"))), 
+        Location.London => TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time").IsDaylightSavingTime(dt) ^ TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time").IsDaylightSavingTime(dt.Subtract(TimeSpan.Parse("7.00:00:00"))), 
+		Location.Paris => TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time").IsDaylightSavingTime(dt) ^ TimeZoneInfo.FindSystemTimeZoneById("W. Europe Standard Time").IsDaylightSavingTime(dt.Subtract(TimeSpan.Parse("7.00:00:00"))) };
 
-    public static DateTime NormalizeDateTime(string dtStr, Location location)
-    {
-        throw new NotImplementedException("Please implement the (static) Appointment.NormalizeDateTime() method");
-    }
+    public static DateTime NormalizeDateTime(string dtStr, Location location) {
+		try {
+			return location switch { Location.NewYork => DateTime.Parse(dtStr, CultureInfo.CreateSpecificCulture("en-US")), 
+	        						 Location.London => DateTime.Parse(dtStr, CultureInfo.CreateSpecificCulture("en-GB")), 
+									 Location.Paris => DateTime.Parse(dtStr, CultureInfo.CreateSpecificCulture("fr-FR")) };
+		} catch(Exception) { return DateTime.MinValue; }	
+	}
 }
