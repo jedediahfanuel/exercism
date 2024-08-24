3bfee880eb7b6f91b8a7d99a03364f2e15a21ac1
jedediahfanuel <jedediahf51@gmail.com>
2024-08-24 22:51:16 +0700

    🫳 :: conundrum calculator done

--- csharp/calculator-conundrum/CalculatorConundrum.cs
+++ csharp/calculator-conundrum/CalculatorConundrum.cs
@@ -1,9 +1,16 @@
-using System;
+using System                                                                                                           ;
 
-public static class SimpleCalculator
-{
-    public static string Calculate(int operand1, int operand2, string operation)
-    {
-        throw new NotImplementedException("Please implement the SimpleCalculator.Calculate() method");
-    }
-}
+public static class SimpleCalculator                                                                                   {
+
+    public static string Calculate(int operand1, int operand2, string operation)                                       {
+
+        if (operand2 == 0) return "Division by zero is not allowed."                                                   ;
+
+        return operation switch                                                                                        {
+             "+" => $"{operand1} {operation} {operand2} = {operand1 + operand2}",
+             "*" => $"{operand1} {operation} {operand2} = {operand1 * operand2}",
+             "/" => $"{operand1} {operation} {operand2} = {operand1 / operand2}",
+              "" => throw new ArgumentException(),
+            null => throw new ArgumentNullException(),
+               _ => throw new ArgumentOutOfRangeException(),
+                                                                                                                       }; } }
