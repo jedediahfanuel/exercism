81b8a0471269f1ac5bfb6685eab000f592f1ac73
jedediahfanuel <jedediahf51@gmail.com>
2024-08-24 23:06:58 +0700

    🤑 :: whitspace

--- csharp/calculator-conundrum/CalculatorConundrum.cs
+++ csharp/calculator-conundrum/CalculatorConundrum.cs
@@ -4,12 +4,12 @@ public static class SimpleCalculator                                            
 
     public static string Calculate(int x, int y, string t)                                                             {
 
-        if (y == 0) return "Division by zero is not allowed."                                                   ;
+        if (y == 0) return "Division by zero is not allowed."                                                          ;
 
-        return t switch                                                                                        {
-             "+" => $"{x} {t} {y} = {x + y}",
-             "*" => $"{x} {t} {y} = {x * y}",
-             "/" => $"{x} {t} {y} = {x / y}",
-              "" => throw new ArgumentException(),
-            null => throw new ArgumentNullException(),
-               _ => throw new ArgumentOutOfRangeException(),                                                           }; } }
+        return t switch                                                                                                {
+             "+" => $"{x} {t} {y} = {x + y}"                ,
+             "*" => $"{x} {t} {y} = {x * y}"                ,
+             "/" => $"{x} {t} {y} = {x / y}"                ,
+              "" => throw new ArgumentException()           ,
+            null => throw new ArgumentNullException()       ,
+               _ => throw new ArgumentOutOfRangeException() ,                                                          }; } }
