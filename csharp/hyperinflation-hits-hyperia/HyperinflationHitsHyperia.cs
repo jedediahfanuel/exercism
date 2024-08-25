534dcd6c10e3363ea181d2a448aae844b50840d9
jedediahfanuel <jedediahf51@gmail.com>
2024-08-25 11:51:30 +0700

    ⚰️  :: remove @ on parameter name

--- csharp/hyperinflation-hits-hyperia/HyperinflationHitsHyperia.cs
+++ csharp/hyperinflation-hits-hyperia/HyperinflationHitsHyperia.cs
@@ -1,10 +1,10 @@
 public static class CentralBank                                                                                                  {
-    public static string DisplayDenomination(long @b, long m)                                                                    {
-        try   { return checked(@b * m).ToString()   ; }
-        catch { return "*** Too Big ***"            ; }                                                                          }
+    public static string DisplayDenomination(long b, long m)                                                                     {
+        try   { return checked(b * m).ToString()   ; }
+        catch { return "*** Too Big ***"           ; }                                                                           }
 
-    public static string DisplayGDP(float @b, float m) => DisplayDenomination((long) @b, (long) m)                               ;
+    public static string DisplayGDP(float b, float m) => DisplayDenomination((long) b, (long) m)                                 ;
 
     public static string DisplayChiefEconomistSalary(decimal s, decimal m)                                                       {
-        try   { return checked(s * m).ToString()    ; }
-        catch { return "*** Much Too Big ***"       ; }                                                                          } }
+        try   { return checked(s * m).ToString()   ; }
+        catch { return "*** Much Too Big ***"      ; }                                                                          } }
