b3447b8ab8f27890fe3c417d1d2c335bc38af3db
jedediahfanuel <jedediahf51@gmail.com>
2024-08-25 13:45:00 +0700

    🚽 :: simplify string formatting

--- csharp/weighing-machine/WeighingMachine.cs
+++ csharp/weighing-machine/WeighingMachine.cs
@@ -6,16 +6,11 @@ class WeighingMachine {
         get { return weight; }
         set { 
             if (value < 0) throw new ArgumentOutOfRangeException();
-            weight = value;
-        } 
-    }
+            weight = value;  }  }
 
     public WeighingMachine(int precision) => Precision = precision;
     public int    Precision      { get; set; }
     public double TareAdjustment { get; set; } = 5d;
 
-    public string DisplayWeight => $"{
-        Math.Round(Weight - TareAdjustment, Precision)           /*rounding*/
-        .ToString("0." + 0.ToString().PadLeft(Precision, '0'))   /*print precision */
-    } kg";
+    public string DisplayWeight => $"{ Math.Round(Weight - TareAdjustment, Precision).ToString($"N{Precision}") } kg";
 }  
