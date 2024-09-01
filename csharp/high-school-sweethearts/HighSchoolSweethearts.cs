f961e3197396bae025a44608aa894d59f7f14af9
jedediahfanuel <jedediahf51@gmail.com>
2024-09-01 11:17:33 +0700

    😈 :: high school sweethearts done

--- csharp/high-school-sweethearts/HighSchoolSweethearts.cs
+++ csharp/high-school-sweethearts/HighSchoolSweethearts.cs
@@ -1,20 +1,27 @@
-using System;
+using System; using System.Globalization;
 
-public static class HighSchoolSweethearts
-{
-    public static string DisplaySingleLine(string studentA, string studentB)
-    {
-        throw new NotImplementedException($"Please implement the (static) HighSchoolSweethearts.DisplaySingleLine() method");
-    }
+public static class HighSchoolSweethearts                                                                                                            {
+    public static string DisplaySingleLine(string studentA, string studentB) =>
+        string.Format("{0,29} {1} {2,-29}", studentA, "♡", studentB);
 
-    public static string DisplayBanner(string studentA, string studentB)
-    {
-        throw new NotImplementedException($"Please implement the (static) HighSchoolSweethearts.DisplayBanner() method");
-    }
+    public static string DisplayBanner(string studentA, string studentB) => $"""
+		     ******       ******
+		   **      **   **      **
+		 **         ** **         **
+		**            *            **
+		**                         **
+		**     {studentA} +  {studentB}    **
+		 **                       **
+		   **                   **
+		     **               **
+		       **           **
+		         **       **
+		           **   **
+		             ***
+		              *
+		""";
 
-    public static string DisplayGermanExchangeStudents(string studentA
-        , string studentB, DateTime start, float hours)
-    {
-        throw new NotImplementedException($"Please implement the (static) HighSchoolSweethearts.DisplayGermanExchangeStudents() method");
-    }
-}
+    public static string DisplayGermanExchangeStudents(string studentA , string studentB, DateTime start, float hours) =>
+        string.Format(new CultureInfo("de-DE"),
+            "{0} and {1} have been dating since {2:d} - that's {3:N2} hours",
+            studentA, studentB, start, hours );                                                                                                      }
