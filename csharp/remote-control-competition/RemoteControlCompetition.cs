837fd8e1295b8b19344f4b54346732620d45b943
jedediahfanuel <jedediahf51@gmail.com>
2024-09-01 13:59:09 +0700

    🀄️ :: direct IComparable

--- csharp/remote-control-competition/RemoteControlCompetition.cs
+++ csharp/remote-control-competition/RemoteControlCompetition.cs
@@ -5,26 +5,20 @@ public interface IRemoteControlCar                                              
     public void Drive();                                                                                                                             }
 
 
-public class ProductionRemoteControlCar : IRemoteControlCar, IComparable                                                                             {
+public class ProductionRemoteControlCar : IRemoteControlCar, IComparable<ProductionRemoteControlCar>                                                 {
     public int DistanceTravelled { get; set; }
     public int NumberOfVictories { get; set; }
-
     public void Drive() => DistanceTravelled += 10;
-
-    public int CompareTo(object other)                                                                                                               {
-        ProductionRemoteControlCar p = other as ProductionRemoteControlCar;
-        return this.NumberOfVictories.CompareTo(p.NumberOfVictories);                                                                                } }
+    public int CompareTo(ProductionRemoteControlCar other) => this.NumberOfVictories.CompareTo(other.NumberOfVictories);                             }
 
 
 public class ExperimentalRemoteControlCar : IRemoteControlCar                                                                                        {
     public int DistanceTravelled { get; set; }
-
     public void Drive() => DistanceTravelled += 20;                                                                                                  }
 
 
 public static class TestTrack {
     public static void Race(IRemoteControlCar car) => car.Drive();
-
     public static List<ProductionRemoteControlCar> 
         GetRankedCars(ProductionRemoteControlCar prc1,ProductionRemoteControlCar prc2) =>
         prc1.CompareTo(prc2) == 1 ? new List<ProductionRemoteControlCar>{prc2, prc1} : new List<ProductionRemoteControlCar>{prc1, prc2};             }
