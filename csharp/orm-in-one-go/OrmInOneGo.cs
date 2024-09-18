4390d954d86011c15665c300877d6fa48dc2651a
jedediahfanuel <jedediahf51@gmail.com>
2024-09-18 20:38:45 +0700

    🩴 :: orm in one go done

--- csharp/orm-in-one-go/OrmInOneGo.cs
+++ csharp/orm-in-one-go/OrmInOneGo.cs
@@ -1,21 +1,26 @@
 using System;
 
-public class Orm
-{
+public class Orm {
     private Database database;
 
-    public Orm(Database database)
-    {
-        this.database = database;
+    public Orm(Database database) => this.database = database;
+
+    public void Write(string data) {
+        using var db = this.database;
+        try {
+            database.BeginTransaction();
+            database.Write(data);
+            database.EndTransaction();
+        }
+        catch (Exception ex) { throw ex;           }
+        finally              { database.Dispose(); }
     }
 
-    public void Write(string data)
-    {
-        throw new NotImplementedException($"Please implement the Orm.Write() method");
-    }
-
-    public bool WriteSafely(string data)
-    {
-        throw new NotImplementedException($"Please implement the Orm.WriteSafely() method");
+    public bool WriteSafely(string data) {
+        try {
+            Write(data);
+            return true;
+        }
+        catch { return false; }
     }
 }
